f7dc1a156c39ded25029a3162e213b4f9b323145
case <case@casesandberg.com>
2016-12-19 16:25:38 -0500

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+var IGNORE = 0
+var WARN = 1
+var ERROR = 2
+
+var ALWAYS = 'always'
+var NEVER = 'never'
+var OFF = 'off'
+
+module.exports = {
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true
+  },
+  rules: {
+    'react/jsx-curly-spacing': [ERROR, ALWAYS, { spacing: { objectLiterals: NEVER }}],
+    "react/jsx-filename-extension": [IGNORE],
+    'react/prop-types': [IGNORE],
+    'template-curly-spacing': [ERROR, ALWAYS],
+    'strict': [IGNORE],
+    'no-unused-expressions': IGNORE,
+    'no-unused-vars': [ERROR, { 'vars': 'local' }],
+    'arrow-body-style': [IGNORE, ALWAYS],
+    'camelcase': [ERROR],
+    'constructor-super': ERROR,
+    'quote-props': [ERROR, 'consistent'],
+    'no-underscore-dangle': [WARN],
+    'semi': [ERROR, NEVER],
+    'import/no-unresolved': [ERROR],
+    'new-cap': [IGNORE],
+    'jsx-a11y/no-static-element-interactions': [IGNORE],
+    'import/no-named-as-default': [IGNORE],
+    'import/no-extraneous-dependencies': [IGNORE],
+    'react/require-extension': OFF
+  },
+  'settings': {
+    'import/core-modules': [
+      'electron',
+      'redux-grpc-middlware',
+      'lightning-notifications',
+      'lightning-store',
+    ]
+  }
+}
