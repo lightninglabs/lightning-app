ab733cd9ec07fd1d79ea2488fe9413f7da08fe3c
case <case@casesandberg.com>
2016-12-19 17:01:26 -0500

    `lightning-webpack` from `lightning-desktop`

---
+++ packages/lightning-webpack/server.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+
+import express from 'express'
+import webpack from 'webpack'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
+
+import config from './webpack.config.development'
+
+const app = express()
+const compiler = webpack(config)
+const PORT = process.env.PORT || 4152
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  noInfo: true,
+}))
+
+app.use(webpackHotMiddleware(compiler))
+
+app.listen(PORT, 'localhost', (err) => {
+  if (err) { console.error(err); return }
+  console.log(`Listening at http://localhost:${ PORT }`)
+})
---
+++ packages/lightning-webpack/webpack.config.base.js
@@ -0,0 +1,52 @@
+import webpack from 'webpack'
+import path from 'path'
+import validate from 'webpack-validator'
+
+export default validate({
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        loaders: ['babel?cacheDirectory'],
+        exclude: /node_modules\/(?!lightning-core|lightning-core-ui)/,
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file',
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+
+    // https://github.com/webpack/webpack/issues/1114
+    // libraryTarget: 'commonjs2',
+  },
+
+  // https://webpack.github.io/docs/configuration.html#resolve
+  resolve: {
+    root: __dirname,
+    extensions: ['', '.js', '.jsx', '.json'],
+    packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main'],
+    alias: {
+      'redux-grpc-middlware': 'packages/redux-grpc-middlware',
+      'lightning-notifications': 'packages/lightning-notifications',
+      'lightning-store': 'packages/lightning-store',
+    },
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({ 'global.GENTLY': false }),
+  ],
+
+  externals: [
+    // put your node 3rd party libraries which can't be built with webpack here
+    // (mysql, mongodb, and so on..)
+  ],
+})
