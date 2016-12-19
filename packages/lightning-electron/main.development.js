9379c12404ce7178289af9b9e88c882215e49a0c
case <case@casesandberg.com>
2016-12-19 17:04:23 -0500

    `lightning-electron` from `lightning-desktop`

---
+++ packages/lightning-electron/main.development.js
@@ -0,0 +1,126 @@
+/* eslint-disable global-require, no-console */
+
+import { app, BrowserWindow } from 'electron'
+import windowStateKeeper from 'electron-window-state'
+import _ from 'lodash'
+import observe from 'observe'
+import cp from 'child_process'
+import ps from 'ps-node'
+import './lnd'
+
+let mainWindow = null
+
+const isProcessRunning = command => new Promise((resolve, reject) => {
+  ps.lookup({ command },
+    (err, resultList) => {
+      if (err) { throw new Error(err) }
+      resultList[0] ? resolve(resultList[0]) : reject()
+    }
+  )
+})
+
+const runProcesses = (processes, logs) => {
+  _.map(processes, (process) => {
+    isProcessRunning(process.name)
+      .then((p) => {
+        console.log(`${ process.name } Already Running`, p)
+      })
+      .catch(() => {
+        const prefix = `${ process.name }: `
+        const instance = cp.execFile(process.name, process.args, { cwd: './bin' }, (error) => {
+          if (error) { logs.push(`${ error.code }: ${ error.errno }`); return }
+        })
+        instance.stdout.on('data', data => logs.push(prefix + data))
+        instance.stderr.on('data', data => logs.push(prefix + data))
+      })
+  })
+}
+
+const logBuffer = []
+const logs = observe(logBuffer)
+
+const processes = [
+  {
+    name: 'lnd',
+    args: [
+      '--btcdhost=127.0.0.1',
+      '--rpcuser=kek',
+      '--rpcpass=kek',
+      '--simnet',
+      '--debuglevel=trace',
+      '--debughtlc',
+    ],
+  }, {
+    name: 'btcd',
+    args: [
+      '--rpcuser=kek',
+      '--rpcpass=kek',
+      '--simnet',
+      '--miningaddr=4NyWssGkW6Nbwj3nXrJU54U2ijHgWaKZ1N19w',
+    ],
+  },
+]
+
+runProcesses(processes, logs)
+
+const createWindow = () => {
+  const mainWindowState = windowStateKeeper({
+    defaultWidth: 750,
+    defaultHeight: 500,
+  })
+
+  const { x, y, width, height } = mainWindowState
+  mainWindow = new BrowserWindow({
+    x,
+    y,
+    width,
+    height,
+    show: false,
+    transparent: true,
+    frame: false,
+    title: 'Lightning',
+  })
+
+  mainWindowState.manage(mainWindow)
+  mainWindow.loadURL(`file://${ __dirname }/app/app.html`)
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.show()
+    mainWindow.focus()
+
+    mainWindow.webContents.send('logs', logBuffer)
+  })
+
+  logs.on('change', (change) => {
+    const log = logBuffer[change.index]
+    mainWindow && mainWindow.webContents.send('log', log)
+  })
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.openDevTools()
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+if (process.env.NODE_ENV === 'development') {
+  require('electron-debug')()
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// if (process.platform === 'darwin') {
+//   const template = [
+//     {
+//       label: app.getName(),
+//     },
+//   ]
+//   const menu = Menu.buildFromTemplate(template)
+//   Menu.setApplicationMenu(menu)
+// }
+
+app.on('ready', createWindow)
---
+++ packages/lightning-electron/package.js
@@ -0,0 +1,157 @@
+/* eslint-disable no-console, consistent-return */
+/* eslint generator-star-spacing: 0 */
+
+import 'babel-polyfill'
+import os from 'os'
+import webpack from 'webpack'
+import packager from 'electron-packager'
+import del from 'del'
+import minimist from 'minimist'
+import { exec } from 'child_process'
+import cfg from './webpack.config.production'
+import pkg from './package.json'
+
+import electronCfg from './webpack.config.electron'
+
+
+/**
+ * First two values are node path and current script path
+ * https://nodejs.org/docs/latest/api/process.html#process_process_argv
+ */
+const argv = minimist(process.argv.slice(2))
+
+/**
+ * Do not package node modules from 'devDependencies'
+ * and 'dependencies' that are set as external
+ */
+const toNodePath = name => `/node_modules/${ name }($|/)`
+const devDeps = Object
+  .keys(pkg.devDependencies)
+  .map(toNodePath)
+
+const depsExternal = Object
+  .keys(pkg.dependencies)
+  .filter(name => !electronCfg.externals.includes(name))
+  .map(toNodePath)
+
+
+const appName = argv.name || argv.n || pkg.productName
+const shouldUseAsar = argv.asar || argv.a || false
+const shouldBuildAll = argv.all || false
+
+
+const DEFAULT_OPTS = {
+  dir: './',
+  name: appName,
+  asar: shouldUseAsar,
+  ignore: [
+    '^/test($|/)',
+    '^/release($|/)',
+    '^/main.development.js',
+  ]
+  .concat(devDeps)
+  .concat(depsExternal),
+}
+
+const icon = argv.icon || argv.i || 'app/app'
+if (icon) DEFAULT_OPTS.icon = icon
+
+
+/**
+ * @desc Execute the webpack build process on given config object
+ * @param {Object} cfg
+ * @return {Promise}
+ */
+function build(config) {
+  return new Promise((resolve, reject) => {
+    webpack(config, (err, stats) => {
+      if (err) return reject(err)
+      resolve(stats)
+    })
+  })
+}
+
+function pack(plat, arch, cb) {
+  // there is no darwin ia32 electron
+  if (plat === 'darwin' && arch === 'ia32') return
+
+  const iconObj = {
+    icon: DEFAULT_OPTS.icon + (() => {
+      let extension = '.png'
+      if (plat === 'darwin') extension = '.icns'
+      if (plat === 'win32') extension = '.ico'
+
+      return extension
+    })(),
+  }
+
+  const opts = Object.assign({}, DEFAULT_OPTS, iconObj, {
+    'platform': plat,
+    arch,
+    'prune': true,
+    'app-version': pkg.version || DEFAULT_OPTS.version,
+    'out': `release/${ plat }-${ arch }`,
+  })
+
+  packager(opts, cb)
+}
+
+
+function log(plat, arch) {
+  return (err, filepath) => {
+    if (err) return console.error(err, filepath)
+    console.log(`${ plat }-${ arch } finished!`)
+  }
+}
+
+
+/** @desc Build, clear previous releases and pack new versions */
+async function startPack() {
+  console.log('start pack...')
+
+  try {
+    /**
+     * - Build the 'Main process' and 'Renderer Process' files.
+     * - Clear the ./release directory
+     */
+    await build(electronCfg)
+    await build(cfg)
+    await del('release')
+
+    // Start the packing process
+    if (shouldBuildAll) {
+      // build for all platforms
+      const archs = ['ia32', 'x64']
+      const platforms = ['linux', 'win32', 'darwin']
+
+      platforms.forEach((plat) => {
+        archs.forEach((arch) => {
+          pack(plat, arch, log(plat, arch))
+        })
+      })
+    } else {
+      // build for current platform only
+      pack(os.platform(), os.arch(), log(os.platform(), os.arch()))
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+
+const version = argv.version || argv.v
+if (version) {
+  DEFAULT_OPTS.version = version
+  startPack()
+} else {
+  // use the same version as the currently-installed electron-prebuilt
+  exec('npm list electron --dev', (err, stdout) => {
+    if (err) {
+      DEFAULT_OPTS.version = '1.2.0'
+    } else {
+      DEFAULT_OPTS.version = stdout.split('electron@')[1].replace(/\s/g, '')
+    }
+
+    startPack()
+  })
+}
