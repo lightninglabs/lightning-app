8946c08048e9357b4cc6c088b341dcbb9d0aece2
case <case@casesandberg.com>
2016-12-21 10:08:39 -0500

    Reconnect GRPC

--- packages/lightning-desktop/lnd.js
+++ packages/lightning-desktop/lnd.js
@@ -1,6 +1,5 @@
-// import grpc from 'grpc'
-//
-// const lnrpc = grpc.load('lnd.proto').lnrpc
-// const lndConn = new lnrpc.Lightning('localhost:10009', grpc.credentials.createInsecure())
-const lndConn = {}
+import grpc from 'grpc'
+
+const lnrpc = grpc.load('../lightning-desktop/lnd.proto').lnrpc
+const lndConn = new lnrpc.Lightning('localhost:10009', grpc.credentials.createInsecure())
 export default lndConn
