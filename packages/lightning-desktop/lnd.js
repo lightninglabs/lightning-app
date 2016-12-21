8082724791f0c47ec51dc8aa6416538b52e23bbf
case <case@casesandberg.com>
2016-12-21 09:54:53 -0500

    Fix Deps, App Loads!

--- packages/lightning-desktop/lnd.js
+++ packages/lightning-desktop/lnd.js
@@ -1,5 +1,6 @@
-import grpc from './bin/grpc'
-
-const lnrpc = grpc.load('lnd.proto').lnrpc
-const lndConn = new lnrpc.Lightning('localhost:10009', grpc.credentials.createInsecure())
+// import grpc from 'grpc'
+//
+// const lnrpc = grpc.load('lnd.proto').lnrpc
+// const lndConn = new lnrpc.Lightning('localhost:10009', grpc.credentials.createInsecure())
+const lndConn = {}
 export default lndConn
