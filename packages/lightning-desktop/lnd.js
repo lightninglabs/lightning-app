38105f1e8cca94199fc9b9b5fd7fb0c435a0a16e
case <case@casesandberg.com>
2017-01-12 14:30:40 -0800

    Update Binaries and `rpc.proto`

--- packages/lightning-desktop/lnd.js
+++ packages/lightning-desktop/lnd.js
@@ -1,5 +1,5 @@
 import grpc from 'grpc'
 
-const lnrpc = grpc.load('../lightning-desktop/lnd.proto').lnrpc
+const lnrpc = grpc.load('../lightning-desktop/rpc.proto').lnrpc
 const lndConn = new lnrpc.Lightning('localhost:10009', grpc.credentials.createInsecure())
 export default lndConn
