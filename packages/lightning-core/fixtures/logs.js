51be13c658915e7e1ab03823ce3105ef9bb5241b
case <case@casesandberg.com>
2017-01-16 10:30:47 -0800

    Remove Fixtures

--- packages/lightning-core/fixtures/logs.js
+++
@@ -1,5 +0,0 @@
-export default [
-  'lnd: 12:51:03 2016-10-09 [INF] LTND: Version 0.1.0-alpha',
-  'lnd: open /Users/case/Library/Application Support/Lnd/lnd.conf: no such file or directory',
-  'lnd: 12:51:52 2016-10-09 [ERR] RPCS: unable to open channel to lightningID(303239306266343534663462393562616639323237383031333031623333316533356434373763366236653766333661353939393833616535383734376233383238) nor peerID(0): unable to find peer lightningID([48 50 57 48 98 102 52 53 52 102 52 98 57 53 98 97 102 57 50 50 55 56 48 49 51 48 49 98 51 51 49 101]), peerID(0)',
-]
--- packages/lightning-core/fixtures/wallet.js
+++
@@ -1,9 +0,0 @@
-export default {
-  id: 0,
-  identity: 'case@casesandberg.com',
-  amount: {
-    blockchain: 149966113712,
-    channels: 3823424212,
-  },
-  address: '1DkyBEKt5S2GDtv7aQw6rQepAvnsRyHoYM',
-}
