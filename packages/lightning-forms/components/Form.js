b64c47f7610ea48679c44149dcef797f819a4020
case <case@casesandberg.com>
2017-01-30 12:25:09 -0800

    Init Form

--- packages/lightning-forms/components/Form.js
+++ packages/lightning-forms/components/Form.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import _ from 'lodash'
 import reactCSS from 'reactcss'
 
-const Form = () => {
-  return (
-    <div>form</div>
-  )
+class Form extends React.Component {
+  compnentDidMount() {
+    const { name, fields, initForm } = this.props
+    initForm(name, _.reduce(fields, (all, field) => {
+      // eslint-disable-next-line no-param-reassign
+      all[field.name] = field.value
+      return all
+    }, {}))
+  }
+
+  render() {
+    const styles = reactCSS({
+      'default': {
+
+      },
+    })
+
+    return (
+      <div>
+        form
+      </div>
+    )
+  }
 }
 
 export default Form
--- packages/lightning-forms/reducer.js
+++ packages/lightning-forms/reducer.js
@@ -1,10 +1,16 @@
 
-export const reducer = () => {
+export const INIT_FORM = 'LIGHTNING_FORMS/INIT_FORM'
 
+export const reducer = (state = {}, action) => {
+  switch (action.type) {
+    case INIT_FORM:
+      return { ...state, [action.form.name]: action.form.data }
+    default: return state
+  }
 }
 
 export const actions = {
-
+  initForm: (name, data) => ({ type: INIT_FORM, form: { name, data } }),
 }
 
 export const selectors = {
