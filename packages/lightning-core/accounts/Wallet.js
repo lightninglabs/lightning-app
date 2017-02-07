3831604fdcff69725c0eb26eeb32e72437cc51cb
case <case@casesandberg.com>
2017-02-07 13:02:34 -0800

    Wallet Header

---
+++ packages/lightning-core/accounts/Wallet.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import _ from 'lodash'
+import reactCSS from 'reactcss'
+
+export const Wallet = () => {
+  const styles = reactCSS({
+    'default': {
+      bg: {
+        background: '#59D9A4',
+        color: '#fff',
+        padding: 30,
+        paddingBottom: 20,
+      },
+      title: {
+        color: 'rgba(0,0,0,0.4)',
+        fontSize: 24,
+      },
+      details: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+      },
+      total: {
+
+      },
+      amount: {
+        fontSize: 32,
+        marginBottom: 4,
+      },
+      address: {
+        fontSize: 16,
+        color: 'rgba(0,0,0,0.4)',
+        width: 250,
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        marginBottom: 10,
+      },
+      breakdown: {
+        fontSize: 16,
+      },
+      item: {
+        display: 'flex',
+        marginBottom: 10,
+      },
+      label: {
+        flex: 1,
+        color: 'rgba(0,0,0,0.4)',
+        whiteSpace: 'nowrap',
+        marginRight: 10,
+      },
+      count: {
+        flex: 1,
+        color: 'reba(255,255,255,0.9)',
+      },
+    },
+  })
+
+  const breakdown = [
+    {
+      label: 'On Chain',
+      amount: '67,122,927',
+    }, {
+      label: 'In Channels',
+      amount: '53,832,471',
+    }, {
+      label: 'Frozen',
+      amount: '2,510',
+    },
+  ]
+
+  return (
+    <div style={ styles.bg }>
+      <div style={ styles.title }>Your Wallet</div>
+      <div style={ styles.details }>
+        <div style={ styles.total }>
+          <div style={ styles.amount }>
+            111,245,170 SAT
+          </div>
+          <div style={ styles.address }>
+            7edb32d4ffd7a7edb32d4ffd7a7f730asd86sa8s65dfgedb
+          </div>
+        </div>
+        <div style={ styles.breakdown }>
+
+          { _.map(breakdown, (item, i) => (
+            <div style={ styles.item } key={ i }>
+              <div style={ styles.label }>
+                { item.label }
+              </div>
+              <div style={ styles.count }>
+                { item.amount }
+              </div>
+            </div>
+          )) }
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Wallet
--- packages/lightning-core/accounts/index.js
+++ packages/lightning-core/accounts/index.js
@@ -1,17 +1,21 @@
 
 import React from 'react'
 import { Head, Page } from '../common'
+import Wallet from './Wallet'
 
 export const Accounts = () => {
   return (
-    <Page>
-      <Head
-        title="Your Channels"
-        body="Channels are like lines of credit you have open with other
-        people on the network."
-      />
-      asd
-    </Page>
+    <div>
+      <Wallet />
+      <Page>
+        <Head
+          title="Your Channels"
+          body="Channels are like lines of credit you have open with other
+          people on the network."
+        />
+        asd
+      </Page>
+    </div>
   )
 }
 
