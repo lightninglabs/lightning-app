731bbfd6aee7bce1ed0141988f0597b4e900e737
case <case@casesandberg.com>
2017-02-15 14:42:54 -0800

    New Channel Design

--- packages/lightning-core/accounts/ChannelList.js
+++ packages/lightning-core/accounts/ChannelList.js
@@ -2,27 +2,73 @@ import React from 'react'
 import _ from 'lodash'
 import reactCSS from 'reactcss'
 
+import { Box } from 'lightning-components'
+
 export const ChannelList = ({ channels }) => {
   const styles = reactCSS({
     'default': {
-      'channel': {
+      channel: {
         borderTop: '1px solid #ddd',
         paddingTop: 20,
         paddingBottom: 20,
+      },
+      split: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginBottom: 7,
+      },
+      id: {
+        color: '#333',
+        fontSize: 20,
+      },
+      status: {
+        fontSize: 13,
+        textTransform: 'uppercase',
+        color: '#999',
+      },
+      local: {
+        fontSize: 16,
+        color: '#4990E2',
+      },
+      remote: {
+        fontSize: 16,
+        color: '#666',
+      },
+      bar: {
+        borderRadius: 2,
+        background: 'lighter-gray',
+        marginTop: 10,
+      },
+      percent: {
+        background: 'blue',
+        height: 12,
+        borderRadius: 2,
       },
     },
   })
   return (
     <div>
-      { _.map(channels, (channel, i) => (
-        <div style={ styles.channel } key={ i }>
-          <div>ID: { channel.id }</div>
-          <div>Local Balance: { channel.localBalance }</div>
-          <div>Remote Balance: { channel.remoteBalance }</div>
-          <div>Capacity: { channel.capacity }</div>
-          <div>Status: { channel.status }</div>
-        </div>
-      )) }
+      { _.map(channels, (channel, i) => {
+        const width = `${ (channel.localBalance / channel.capacity) * 100 }%`
+        return (
+          <div style={ styles.channel } key={ i }>
+            <div style={ styles.split }>
+              <div style={ styles.id }>{ channel.id }</div>
+              <div style={ styles.status }>{ channel.status }</div>
+            </div>
+
+            <div style={ styles.split }>
+              <div style={ styles.local }>My Balance: { channel.localBalance }</div>
+              <div style={ styles.remote }>Avaliable to Recieve: { channel.remoteBalance }</div>
+            </div>
+
+            <Box style={ styles.bar }>
+              <Box style={ styles.percent } width={ width } />
+            </Box>
+
+          </div>
+        )
+      }) }
     </div>
   )
 }
--- packages/lightning-core/accounts/index.js
+++ packages/lightning-core/accounts/index.js
@@ -7,14 +7,14 @@ import ChannelList from './ChannelList'
 const channels = [
   {
     remotePubkey: 'af828g32gh7a5g72ds45fg24sa35gra',
-    id: '0dfg86z5f3g546s3f5r78ga76gs75d6',
+    id: '1195695805464576000',
     capacity: 40000,
     localBalance: 10000,
     remoteBalance: 30000,
     status: 'active',
   }, {
     remotePubkey: 'gra0dfg86z5f3g546s3f5r78ga76afas',
-    id: '72ds45fg24sa35gs75d6af828g32gh7a5g',
+    id: '8054645119526957621',
     capacity: 20000,
     localBalance: 17000,
     remoteBalance: 3000,
