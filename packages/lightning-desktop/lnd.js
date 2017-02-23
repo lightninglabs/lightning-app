f1261c1c8b3a129d58c5264c7345cd56241c5040
case <case@casesandberg.com>
2017-02-22 17:38:51 -0800

    Get GRPC Working Again

--- packages/lightning-desktop/lnd.js
+++ packages/lightning-desktop/lnd.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-var */
-// var grpc = require('grpc')
-//
-// var lnrpc = grpc.load('../lightning-desktop/rpc.proto').lnrpc
-// var lndConn = new lnrpc.Lightning('localhost:10009', grpc.credentials.createInsecure())
+var grpc = require('grpc')
+
+var lnrpc = grpc.load('../lightning-desktop/rpc.proto').lnrpc
+var lndConn = new lnrpc.Lightning('localhost:10009', grpc.credentials.createInsecure())
 
 module.exports = {
-  default: {},
+  default: lndConn,
 }
