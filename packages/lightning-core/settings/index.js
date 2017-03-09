a7452ec2a3464147a701dd25981d4d5b435d19f0
case <case@casesandberg.com>
2017-03-08 17:33:04 -0800

    Put Back Settings Page

--- packages/lightning-core/settings/index.js
+++ packages/lightning-core/settings/index.js
@@ -1,20 +1,74 @@
 import React from 'react'
+import _ from 'lodash'
 import reactCSS from 'reactcss'
+import { connect } from 'react-redux'
+import { store } from 'lightning-store'
 
-export const SettingsPage = () => {
+import { Box, Text } from 'lightning-components'
+
+export const SettingsPage = ({ logs, pubkey }) => {
   const styles = reactCSS({
-    defaut: {
+    'default': {
       page: {
-
+        boxSizing: 'border-box',
+        padding: 'large',
+        direction: 'column',
+        minWidth: 0,
+        flex: 1,
+      },
+      title: {
+        size: 'medium',
+        paddingBottom: 'medium',
+        color: 'gray',
+      },
+      account: {
+        paddingBottom: 'large',
+      },
+      logs: {
+        flex: 1,
+        padding: 'medium',
+        overflowX: 'auto',
+        WebkitAppRegion: 'no-drag',
+        zDepth: 1,
+        width: '100%',
+        boxSizing: 'border-box',
+        background: 'white',
+        marginBottom: 'medium',
+      },
+      log: {
+        size: 'small',
+        color: 'gray',
+        fontFamily: 'monospace',
       },
     },
   })
 
+
   return (
-    <div style={ styles.page }>
-      SettingsPage
-    </div>
+    <Box style={ styles.page }>
+      { pubkey ? (
+        <Box style={ styles.account }>
+          <Text size="medium" display="block" ellipsis><Text bold>Pubkey: </Text>{ pubkey }</Text>
+        </Box>
+      ) : null }
+
+      <Text { ...styles.title }>Logs</Text>
+      <Box style={ styles.logs }>
+        { _.map(logs, (log, i) => {
+          return (
+            <div key={ i } style={{ whiteSpace: 'nowrap' }}>
+              <Text { ...styles.log }>{ log }</Text>
+            </div>
+          )
+        }) }
+      </Box>
+    </Box>
   )
 }
 
-export default SettingsPage
+export default connect(
+  state => ({
+    logs: store.getRecentLogs(state),
+    pubkey: store.getAccountPubkey(state),
+  })
+)(SettingsPage)
