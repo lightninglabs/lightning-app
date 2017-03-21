7a8d2998aac082d32852cfd477f7feca0c17c015
case <case@casesandberg.com>
2017-03-21 16:48:48 -0700

    Fix Spawn Process Path

--- packages/lightning-desktop/main.development.js
+++ packages/lightning-desktop/main.development.js
@@ -26,7 +26,7 @@ const runProcesses = (processes, logs) => {
       })
       .catch(() => {
         const prefix = `${ process.name }: `
-        const instance = cp.execFile(process.name, process.args, { cwd: '../lightning-desktop/bin' }, (error) => {
+        const instance = cp.execFile(`../lightning-desktop/bin/${ process.name }`, process.args, (error) => {
           if (error) { logs.push(`${ error.code }: ${ error.errno }`); return }
         })
         instance.stdout.on('data', data => logs.push(prefix + data))
