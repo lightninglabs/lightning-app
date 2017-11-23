56e85394249ee1d84556ab525539a5e79faefdd5
Kevin Johnson <kevinejohn@gmail.com>
2017-11-22 18:15:23 -0800

    prettier

--- public/electron.js
+++ public/electron.js
@@ -1,27 +1,29 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
-const isDev = require('electron-is-dev')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 750, height: 500})
+  win = new BrowserWindow({ width: 750, height: 500 });
 
   if (isDev) {
-    win.loadURL('http://localhost:3000')
+    win.loadURL('http://localhost:3000');
 
     // Open the DevTools.
     // win.webContents.openDevTools()
   } else {
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, '..', 'build', 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }))
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, '..', 'build', 'index.html'),
+        protocol: 'file:',
+        slashes: true,
+      })
+    );
   }
 
   // Emitted when the window is closed.
@@ -29,31 +31,31 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
-  })
+    win = null;
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React, { Component } from 'react';
 import Main from './views/main';
-// import reactCSS from 'reactcss';
 
 class App extends Component {
   render() {
     return <Main />;
-
-    // const styles = reactCSS({
-    //   default: {
-    //     container: {
-    //
-    //     },
-    //   },
-    // });
-    //
-    //
-    // return (
-    //   <div style={styles.container}>
-    //     <Main />
-    //   </div>
-    // );
   }
 }
 
