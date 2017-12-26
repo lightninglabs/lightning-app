3f6a3909a0e05c4c67117f5fb04d75a8650fff9b
Tankred Hase <mail@tankredhase.de>
2017-12-26 20:49:43 +0100

    Cleanup separator styles

--- src/components/seperator.js
+++ src/components/seperator.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { View } from 'react-native';
 import { colors } from '../styles';
 
+const styles = {
+  seperator: {
+    height: 1,
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: colors.lightestgray
+  }
+}
+
 const Seperator = () => (
-  <View
-    style={{
-      height: 1,
-      marginTop: 20,
-      marginBottom: 20,
-      backgroundColor: colors.lightestgray,
-    }}
-  />
+  <View style={styles.seperator} />
 )
 
 export default Seperator;
