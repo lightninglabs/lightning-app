ff1fc5d59de748a768f1a2544b47e5682e132287
Tankred Hase <mail@tankredhase.de>
2018-01-25 12:22:35 -0500

    Add unit tests for getChannels, getPendingChannels actions

--- test/unit/actions/channels.spec.js
+++ test/unit/actions/channels.spec.js
@@ -12,7 +12,7 @@ describe('Actions Channels Unit Tests', () => {
     store = observable({
       lndReady: false,
     });
-
+    require('../../../src/config').RETRY_DELAY = 0;
     actionsGrpc = sinon.createStubInstance(ActionsGrpc);
     actionsGrpc.sendCommand.resolves({});
     actionsChannels = new ActionsChannels(store, actionsGrpc);
@@ -26,6 +26,46 @@ describe('Actions Channels Unit Tests', () => {
       expect(actionsGrpc.sendCommand, 'was called with', 'listChannels');
       expect(actionsGrpc.sendCommand, 'was called with', 'pendingChannels');
       expect(actionsGrpc.sendCommand, 'was called with', 'listPeers');
+    });
+  });
+
+  describe('getChannels()', () => {
+    it('should list open channels', async () => {
+      actionsGrpc.sendCommand.withArgs('listChannels').resolves({
+        channels: [{ chan_id: 42, active: true }],
+      });
+      await actionsChannels.getChannels();
+      expect(store.channelsResponse, 'to satisfy', [
+        { id: 42, status: 'open' },
+      ]);
+    });
+
+    it('should retry on failure', async () => {
+      actionsGrpc.sendCommand.onFirstCall().rejects();
+      await actionsChannels.getChannels();
+      actionsGrpc.sendCommand.resolves({});
+      await new Promise(resolve => setTimeout(resolve, 1));
+      expect(actionsGrpc.sendCommand, 'was called twice');
+    });
+  });
+
+  describe('getPendingChannels()', () => {
+    it('should list open channels', async () => {
+      actionsGrpc.sendCommand.withArgs('pendingChannels').resolves({
+        pending_open_channels: [{}],
+        pending_closing_channels: [{}],
+        pending_force_closing_channels: [{}],
+      });
+      await actionsChannels.getPendingChannels();
+      expect(store.pendingChannelsResponse.length, 'to equal', 3);
+    });
+
+    it('should retry on failure', async () => {
+      actionsGrpc.sendCommand.onFirstCall().rejects();
+      await actionsChannels.getPendingChannels();
+      actionsGrpc.sendCommand.resolves({});
+      await new Promise(resolve => setTimeout(resolve, 1));
+      expect(actionsGrpc.sendCommand, 'was called twice');
     });
   });
 });
