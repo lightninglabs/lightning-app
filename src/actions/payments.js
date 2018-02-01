aea1b83f14b732223b6fa965d788b391323e1f22
Tankred Hase <mail@tankredhase.de>
2018-02-01 17:20:16 -0500

    Test payment actions and refactor to async/await

--- src/actions/payments.js
+++ src/actions/payments.js
@@ -1,4 +1,3 @@
-import { observe } from 'mobx';
 import { PREFIX_URI } from '../config';
 import * as log from './logs';
 
@@ -7,80 +6,57 @@ class ActionsPayments {
     this._store = store;
     this._actionsGrpc = actionsGrpc;
     this._actionsWallet = actionsWallet;
-    observe(this._store, 'lndReady', () => {});
   }
 
-  makePayment({ payment, amount }) {
-    return new Promise((resolve, reject) => {
-      this.decodePaymentRequest(payment)
-        .then(() => {
-          this.payLightning(payment)
-            .then(resolve)
-            .catch(reject);
-        })
-        .catch(err => {
-          log.info('ActionsPayments makePayment', err);
-          // Pay to coin
-          this.sendCoins({
-            addr: payment,
-            amount,
-          })
-            .then(resolve)
-            .catch(reject);
-        });
+  async makePayment({ payment, amount }) {
+    try {
+      await this.decodePaymentRequest(payment);
+    } catch (err) {
+      log.info('ActionsPayments makePayment', err);
+      this.sendCoins({ addr: payment, amount });
+      return;
+    }
+    await this.payLightning(payment);
+  }
+
+  async sendCoins({ addr, amount }) {
+    await this._actionsGrpc.sendCommand('sendCoins', {
+      addr,
+      amount,
     });
+    this._actionsWallet.updateBalances();
   }
 
-  sendCoins({ addr, amount }) {
-    // Send to coin address
-    return new Promise((resolve, reject) => {
-      return this._actionsGrpc
-        .sendCommand('sendCoins', {
-          addr,
-          amount,
-        })
-        .then(() => {
-          this._actionsWallet.updateBalances();
+  async payLightning(payment) {
+    payment = payment.replace(PREFIX_URI, ''); // Remove URI prefix if it exists
+    const payments = await this._actionsGrpc.sendStreamCommand('sendPayment');
+    await new Promise((resolve, reject) => {
+      payments.on('data', data => {
+        if (data.payment_error === '') {
           resolve();
-        })
-        .catch(reject);
+        } else {
+          reject(new Error('Payment route failure'));
+        }
+      });
+      payments.on('error', reject);
+      payments.write({ payment_request: payment });
     });
   }
 
-  payLightning(payment) {
-    return new Promise((resolve, reject) => {
-      payment = payment.replace(PREFIX_URI, ''); // Remove URI prefix if it exists
-      this._actionsGrpc
-        .sendStreamCommand('sendPayment')
-        .then(payments => {
-          payments.on('data', data => {
-            if (data.payment_error === '') {
-              resolve();
-            } else {
-              reject(new Error('Payment route failure'));
-            }
-          });
-          payments.on('error', reject);
-          payments.write({ payment_request: payment });
-        })
-        .catch(reject);
-    });
-  }
-
-  async decodePaymentRequest(paymentRequest) {
-    // Check if lightning address
-    paymentRequest = paymentRequest.replace(PREFIX_URI, ''); // Remove URI prefix if it exists
+  async decodePaymentRequest(payment) {
+    payment = payment.replace(PREFIX_URI, ''); // Remove URI prefix if it exists
     try {
       const request = await this._actionsGrpc.sendCommand('decodePayReq', {
-        pay_req: paymentRequest,
+        pay_req: payment,
       });
       this._store.paymentRequestResponse = {
         numSatoshis: request.num_satoshis,
         description: request.description,
       };
-    } catch (e) {
+    } catch (err) {
       this._store.paymentRequestResponse = {};
-      log.error(e);
+      log.error(err);
+      throw err;
     }
   }
 }
--- test/unit/actions/payments.spec.js
+++ test/unit/actions/payments.spec.js
@@ -1,22 +1,99 @@
 import { observable, useStrict } from 'mobx';
 import ActionsGrpc from '../../../src/actions/grpc';
+import ActionsWallet from '../../../src/actions/wallet';
 import ActionsPayments from '../../../src/actions/payments';
+import * as logger from '../../../src/actions/logs';
 
 describe('Actions Payments Unit Tests', () => {
   let store;
+  let sandbox;
   let actionsGrpc;
+  let actionsWallet;
   let actionsPayments;
 
   beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    sandbox.stub(logger);
     useStrict(false);
-    store = observable({
-      lndReady: false,
-    });
+    store = observable({ lndReady: false });
     require('../../../src/config').RETRY_DELAY = 1;
     actionsGrpc = sinon.createStubInstance(ActionsGrpc);
-    actionsGrpc.sendCommand.resolves({ yes: true });
-    store.lndReady = true;
-    actionsPayments = new ActionsPayments(store, actionsGrpc);
+    actionsWallet = sinon.createStubInstance(ActionsWallet);
+    actionsPayments = new ActionsPayments(store, actionsGrpc, actionsWallet);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('makePayment()', () => {
+    it('should send payment', async () => {
+      actionsGrpc.sendCommand.withArgs('decodePayReq').resolves({});
+      const paymentsOnStub = sinon
+        .stub()
+        .withArgs('data')
+        .yields({ payment_error: '' });
+      const paymentsWriteStub = sinon.stub();
+      actionsGrpc.sendStreamCommand.withArgs('sendPayment').resolves({
+        on: paymentsOnStub,
+        write: paymentsWriteStub,
+      });
+
+      await actionsPayments.makePayment({
+        payment: 'some-payment',
+        amount: 'some-amount',
+      });
+
+      expect(actionsGrpc.sendCommand, 'was called once');
+      expect(actionsGrpc.sendCommand, 'was called with', 'decodePayReq');
+      expect(actionsGrpc.sendStreamCommand, 'was called with', 'sendPayment');
+      expect(paymentsWriteStub, 'was called with', {
+        payment_request: 'some-payment',
+      });
+      expect(actionsWallet.updateBalances, 'was not called');
+    });
+
+    it('should throw error if send payment fails', async () => {
+      actionsGrpc.sendCommand.withArgs('decodePayReq').resolves({});
+      const paymentsOnStub = sinon
+        .stub()
+        .withArgs('data')
+        .yields({ payment_error: 'Boom!' });
+      const paymentsWriteStub = sinon.stub();
+      actionsGrpc.sendStreamCommand.withArgs('sendPayment').resolves({
+        on: paymentsOnStub,
+        write: paymentsWriteStub,
+      });
+
+      await expect(
+        actionsPayments.makePayment({
+          payment: 'some-payment',
+          amount: 'some-amount',
+        }),
+        'to be rejected with error satisfying',
+        /Payment route failure/
+      );
+    });
+
+    it('should send to coin address if decoding payment request failed', async () => {
+      actionsGrpc.sendCommand
+        .withArgs('decodePayReq')
+        .rejects(new Error('Boom!'));
+      actionsGrpc.sendCommand.withArgs('sendCoins').resolves();
+
+      await actionsPayments.makePayment({
+        payment: 'some-payment',
+        amount: 'some-amount',
+      });
+
+      expect(actionsGrpc.sendCommand, 'was called with', 'decodePayReq');
+      expect(actionsGrpc.sendStreamCommand, 'was not called');
+      expect(actionsGrpc.sendCommand, 'was called with', 'sendCoins', {
+        addr: 'some-payment',
+        amount: 'some-amount',
+      });
+      expect(actionsWallet.updateBalances, 'was called once');
+    });
   });
 
   describe('decodePaymentRequest()', () => {
@@ -35,7 +112,9 @@ describe('Actions Payments Unit Tests', () => {
         cltv_expiry: '9',
       };
       actionsGrpc.sendCommand.withArgs('decodePayReq').resolves(response);
+
       await actionsPayments.decodePaymentRequest('goodPaymentRequest');
+
       expect(
         store.paymentRequestResponse.numSatoshis,
         'to be',
@@ -46,6 +125,21 @@ describe('Actions Payments Unit Tests', () => {
         'to be',
         response.description
       );
+    });
+
+    it('should reset state in case of failure', async () => {
+      actionsGrpc.sendCommand
+        .withArgs('decodePayReq')
+        .rejects(new Error('Boom!'));
+
+      await expect(
+        actionsPayments.decodePaymentRequest('goodPaymentRequest'),
+        'to be rejected with error satisfying',
+        /Boom/
+      );
+
+      expect(store.paymentRequestResponse.numSatoshis, 'to be', undefined);
+      expect(store.paymentRequestResponse.description, 'to be', undefined);
     });
   });
 });
