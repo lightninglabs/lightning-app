74691a5f686ff75d67c7ca3e7bd6b26458dc581c
Tankred Hase <mail@tankredhase.de>
2018-02-02 11:26:25 -0500

    Unit test and refactor transactions actions

--- src/actions/transactions.js
+++ src/actions/transactions.js
@@ -10,100 +10,90 @@ class ActionsTransactions {
       this.getTransactions();
       this.getInvoices();
       this.getPayments();
-
       // this.subscribeTransactions();
       // this.subscribeInvoices();
     });
   }
 
-  getTransactions() {
-    this._actionsGrpc
-      .sendCommand('getTransactions')
-      .then(response => {
-        this._store.transactionsResponse = response.transactions.map(
-          transaction => ({
-            id: transaction.tx_hash,
-            type: 'bitcoin',
-            amount: transaction.amount,
-            status:
-              transaction.num_confirmations < 1 ? 'unconfirmed' : 'confirmed',
-            date: new Date(parseInt(transaction.time_stamp, 10)),
-            hash: transaction.tx_hash,
-          })
-        );
-      })
-      .catch(() => {
-        clearTimeout(this.tgetTransactions);
-        this.tgetTransactions = setTimeout(
-          () => this.getTransactions(),
-          RETRY_DELAY
-        );
-      });
+  async getTransactions() {
+    try {
+      const { transactions } = await this._actionsGrpc.sendCommand(
+        'getTransactions'
+      );
+      this._store.transactionsResponse = transactions.map(transaction => ({
+        id: transaction.tx_hash,
+        type: 'bitcoin',
+        amount: transaction.amount,
+        status: transaction.num_confirmations < 1 ? 'unconfirmed' : 'confirmed',
+        date: new Date(parseInt(transaction.time_stamp, 10)),
+        hash: transaction.tx_hash,
+      }));
+    } catch (err) {
+      clearTimeout(this.tgetTransactions);
+      this.tgetTransactions = setTimeout(
+        () => this.getTransactions(),
+        RETRY_DELAY
+      );
+    }
   }
 
-  getInvoices() {
-    this._actionsGrpc
-      .sendCommand('listInvoices')
-      .then(response => {
-        this._store.invoicesResponse = response.invoices.map(invoice => ({
-          id: invoice.creation_date,
-          type: 'lightning',
-          amount: invoice.value,
-          status: invoice.settled ? 'complete' : 'in-progress',
-          date: new Date(parseInt(invoice.creation_date, 10)),
-          memo: invoice.memo,
-          hash: toHash(invoice.r_preimage),
-        }));
-      })
-      .catch(() => {
-        clearTimeout(this.tgetInvoices);
-        this.tgetInvoices = setTimeout(() => this.getInvoices(), RETRY_DELAY);
-      });
+  async getInvoices() {
+    try {
+      const { invoices } = await this._actionsGrpc.sendCommand('listInvoices');
+      this._store.invoicesResponse = invoices.map(invoice => ({
+        id: invoice.creation_date,
+        type: 'lightning',
+        amount: invoice.value,
+        status: invoice.settled ? 'complete' : 'in-progress',
+        date: new Date(parseInt(invoice.creation_date, 10)),
+        memo: invoice.memo,
+        hash: toHash(invoice.r_preimage),
+      }));
+    } catch (err) {
+      clearTimeout(this.tgetInvoices);
+      this.tgetInvoices = setTimeout(() => this.getInvoices(), RETRY_DELAY);
+    }
   }
 
-  getPayments() {
-    this._actionsGrpc
-      .sendCommand('listPayments')
-      .then(response => {
-        this._store.paymentsResponse = response.payments.map(payment => ({
-          id: payment.creation_date,
-          type: 'lightning',
-          amount: payment.value,
-          status: 'complete',
-          date: new Date(parseInt(payment.creation_date, 10)),
-          hash: payment.payment_hash,
-        }));
-      })
-      .catch(() => {
-        clearTimeout(this.tgetPayments);
-        this.tgetPayments = setTimeout(() => this.getPayments(), RETRY_DELAY);
-      });
+  async getPayments() {
+    try {
+      const { payments } = await this._actionsGrpc.sendCommand('listPayments');
+      this._store.paymentsResponse = payments.map(payment => ({
+        id: payment.creation_date,
+        type: 'lightning',
+        amount: payment.value,
+        status: 'complete',
+        date: new Date(parseInt(payment.creation_date, 10)),
+        hash: payment.payment_hash,
+      }));
+    } catch (err) {
+      clearTimeout(this.tgetPayments);
+      this.tgetPayments = setTimeout(() => this.getPayments(), RETRY_DELAY);
+    }
   }
 
-  subscribeTransactions() {
-    this._actionsGrpc
-      .sendStreamCommand('subscribeTransactions')
-      .then(() => {})
-      .catch(() => {
-        clearTimeout(this.tsubscribeTransactions);
-        this.tsubscribeTransactions = setTimeout(
-          () => this.subscribeTransactions(),
-          RETRY_DELAY
-        );
-      });
+  async subscribeTransactions() {
+    try {
+      await this._actionsGrpc.sendStreamCommand('subscribeTransactions');
+    } catch (err) {
+      clearTimeout(this.tsubscribeTransactions);
+      this.tsubscribeTransactions = setTimeout(
+        () => this.subscribeTransactions(),
+        RETRY_DELAY
+      );
+    }
   }
 
-  subscribeInvoices() {
-    this._actionsGrpc
-      .sendStreamCommand('subscribeInvoices')
-      .then(() => {})
-      .catch(() => {
-        clearTimeout(this.tsubscribeInvoices);
-        this.tsubscribeInvoices = setTimeout(
-          () => this.subscribeInvoices(),
-          RETRY_DELAY
-        );
-      });
+  async subscribeInvoices() {
+    try {
+      await this._actionsGrpc.sendStreamCommand('subscribeInvoices');
+    } catch (err) {
+      clearTimeout(this.tsubscribeInvoices);
+      this.tsubscribeInvoices = setTimeout(
+        () => this.subscribeInvoices(),
+        RETRY_DELAY
+      );
+    }
   }
 }
 
---
+++ test/unit/actions/transactions.spec.js
@@ -0,0 +1,166 @@
+import { observable, useStrict } from 'mobx';
+import ActionsGrpc from '../../../src/actions/grpc';
+import ActionsTransactions from '../../../src/actions/transactions';
+import * as logger from '../../../src/actions/logs';
+
+describe('Actions Transactions Unit Tests', () => {
+  let store;
+  let sandbox;
+  let actionsGrpc;
+  let actionsTransactions;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    sandbox.stub(logger);
+    useStrict(false);
+    store = observable({ lndReady: false });
+    require('../../../src/config').RETRY_DELAY = 1;
+    actionsGrpc = sinon.createStubInstance(ActionsGrpc);
+    actionsTransactions = new ActionsTransactions(store, actionsGrpc);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('constructor()', () => {
+    it('should get transactions, invoices, payments on lndReady', () => {
+      actionsGrpc.sendCommand.resolves({});
+      expect(actionsGrpc.sendCommand, 'was not called');
+      store.lndReady = true;
+      expect(actionsGrpc.sendCommand, 'was called with', 'getTransactions');
+      expect(actionsGrpc.sendCommand, 'was called with', 'listInvoices');
+      expect(actionsGrpc.sendCommand, 'was called with', 'listPayments');
+    });
+  });
+
+  describe('getTransactions()', () => {
+    it('should set transaction response in store', async () => {
+      actionsGrpc.sendCommand.withArgs('getTransactions').resolves({
+        transactions: [
+          {
+            tx_hash: 'some-hash',
+            amount: 42,
+            num_confirmations: 6,
+            time_stamp: 1517585154925,
+          },
+        ],
+      });
+      await actionsTransactions.getTransactions();
+      expect(store.transactionsResponse[0], 'to equal', {
+        id: 'some-hash',
+        type: 'bitcoin',
+        amount: 42,
+        status: 'confirmed',
+        date: new Date('Fri, 02 Feb 2018 15:25:54.925 GMT'),
+        hash: 'some-hash',
+      });
+    });
+
+    it('should retry on failure', async () => {
+      actionsGrpc.sendCommand.onFirstCall().rejects();
+      await actionsTransactions.getTransactions();
+      actionsGrpc.sendCommand.resolves({});
+      await new Promise(resolve => setTimeout(resolve, 30));
+      expect(actionsGrpc.sendCommand.callCount, 'to be greater than', 1);
+    });
+  });
+
+  describe('getInvoices()', () => {
+    it('should set transaction response in store', async () => {
+      actionsGrpc.sendCommand.withArgs('listInvoices').resolves({
+        invoices: [
+          {
+            creation_date: 1517585154925,
+            value: 42,
+            settled: true,
+            memo: 'some-memo',
+            r_preimage: 'some-preimage',
+          },
+        ],
+      });
+      await actionsTransactions.getInvoices();
+      expect(store.invoicesResponse[0], 'to equal', {
+        id: 1517585154925,
+        type: 'lightning',
+        amount: 42,
+        status: 'complete',
+        date: new Date('Fri, 02 Feb 2018 15:25:54.925 GMT'),
+        memo: 'some-memo',
+        hash: 'b2899efa9ade8a66a0',
+      });
+    });
+
+    it('should retry on failure', async () => {
+      actionsGrpc.sendCommand.onFirstCall().rejects();
+      await actionsTransactions.getInvoices();
+      actionsGrpc.sendCommand.resolves({});
+      await new Promise(resolve => setTimeout(resolve, 30));
+      expect(actionsGrpc.sendCommand.callCount, 'to be greater than', 1);
+    });
+  });
+
+  describe('getPayments()', () => {
+    it('should set transaction response in store', async () => {
+      actionsGrpc.sendCommand.withArgs('listPayments').resolves({
+        payments: [
+          {
+            creation_date: 1517585154925,
+            value: 42,
+            settled: true,
+            payment_hash: 'some-hash',
+          },
+        ],
+      });
+      await actionsTransactions.getPayments();
+      expect(store.paymentsResponse[0], 'to equal', {
+        id: 1517585154925,
+        type: 'lightning',
+        amount: 42,
+        status: 'complete',
+        date: new Date('Fri, 02 Feb 2018 15:25:54.925 GMT'),
+        hash: 'some-hash',
+      });
+    });
+
+    it('should retry on failure', async () => {
+      actionsGrpc.sendCommand.onFirstCall().rejects();
+      await actionsTransactions.getPayments();
+      actionsGrpc.sendCommand.resolves({});
+      await new Promise(resolve => setTimeout(resolve, 30));
+      expect(actionsGrpc.sendCommand.callCount, 'to be greater than', 1);
+    });
+  });
+
+  describe('subscribeTransactions()', () => {
+    it('should set transaction response in store', async () => {
+      actionsGrpc.sendStreamCommand
+        .withArgs('subscribeTransactions')
+        .resolves();
+      await actionsTransactions.subscribeTransactions();
+    });
+
+    it('should retry on failure', async () => {
+      actionsGrpc.sendStreamCommand.onFirstCall().rejects();
+      await actionsTransactions.subscribeTransactions();
+      actionsGrpc.sendStreamCommand.resolves({});
+      await new Promise(resolve => setTimeout(resolve, 30));
+      expect(actionsGrpc.sendStreamCommand.callCount, 'to be greater than', 1);
+    });
+  });
+
+  describe('subscribeInvoices()', () => {
+    it('should set transaction response in store', async () => {
+      actionsGrpc.sendStreamCommand.withArgs('subscribeInvoices').resolves();
+      await actionsTransactions.subscribeInvoices();
+    });
+
+    it('should retry on failure', async () => {
+      actionsGrpc.sendStreamCommand.onFirstCall().rejects();
+      await actionsTransactions.subscribeInvoices();
+      actionsGrpc.sendStreamCommand.resolves({});
+      await new Promise(resolve => setTimeout(resolve, 30));
+      expect(actionsGrpc.sendStreamCommand.callCount, 'to be greater than', 1);
+    });
+  });
+});
