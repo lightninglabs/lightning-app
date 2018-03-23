580dfd97c303532ff7061297b9782223729c982c
Tankred Hase <mail@tankredhase.de>
2018-03-24 01:38:51 +0200

    Add other font-faces

--- src/component/text.js
+++ src/component/text.js
@@ -3,15 +3,32 @@ import { Text as RNText, TextPropTypes } from 'react-native';
 import PropTypes from 'prop-types';
 import { colors } from './styles';
 
-import '../../assets/font/OpenSans-Regular.ttf';
-import '../../assets/font/OpenSans-Bold.ttf';
 import '../../assets/font/OpenSans-Light.ttf';
-import '../../assets/font/Poppins-Regular.ttf';
-import '../../assets/font/Poppins-Bold.ttf';
+import '../../assets/font/OpenSans-Regular.ttf';
+import '../../assets/font/OpenSans-SemiBold.ttf';
+import '../../assets/font/OpenSans-Bold.ttf';
+import '../../assets/font/OpenSans-ExtraBold.ttf';
+
+import '../../assets/font/Poppins-Thin.ttf';
+import '../../assets/font/Poppins-ExtraLight.ttf';
 import '../../assets/font/Poppins-Light.ttf';
-import '../../assets/font/WorkSans-Regular.ttf';
-import '../../assets/font/WorkSans-Bold.ttf';
+import '../../assets/font/Poppins-Regular.ttf';
+import '../../assets/font/Poppins-Medium.ttf';
+import '../../assets/font/Poppins-SemiBold.ttf';
+import '../../assets/font/Poppins-Bold.ttf';
+import '../../assets/font/Poppins-ExtraBold.ttf';
+import '../../assets/font/Poppins-Black.ttf';
+
+import '../../assets/font/WorkSans-Thin.ttf';
+import '../../assets/font/WorkSans-ExtraLight.ttf';
 import '../../assets/font/WorkSans-Light.ttf';
+import '../../assets/font/WorkSans-Regular.ttf';
+import '../../assets/font/WorkSans-Medium.ttf';
+import '../../assets/font/WorkSans-SemiBold.ttf';
+import '../../assets/font/WorkSans-Bold.ttf';
+import '../../assets/font/WorkSans-ExtraBold.ttf';
+import '../../assets/font/WorkSans-Black.ttf';
+
 import './font.css';
 
 const styles = {
