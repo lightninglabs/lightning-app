b6146137008b6355a18420f969037db929be0d64
Tankred Hase <mail@tankredhase.de>
2018-03-26 16:34:06 +0200

    Remove welcome text in storybook

--- stories/index.js
+++ stories/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-
-import { Welcome } from '@storybook/react/demo';
 
 import Container from '../src/component/container';
 import MainContent from '../src/component/main-content';
@@ -12,10 +9,6 @@ import Header from '../src/component/header';
 import Button from '../src/component/button';
 import Text from '../src/component/text';
 import { colors } from '../src/component/styles';
-
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-));
 
 storiesOf('Colors', module)
   .add('Primary Purple', () => (
