3b3395f0688377040afa9ed5fed125ff0686d51a
Tankred Hase <mail@tankredhase.de>
2018-03-26 12:17:14 +0200

    Add more colors to styles.js

--- src/component/styles.js
+++ src/component/styles.js
@@ -1,8 +1,11 @@
 export const colors = {
   white: '#FFFFFF',
   purple: '#57038D',
+  orange: '#F66B1C',
+  dark: '#252F4A',
   glas: 'rgba(255,255,255,0.13)',
   glasDark: 'rgba(255,255,255,0.26)',
   purpleGradient:
     'linear-gradient(132.27deg, #A95BDC 0%, #651399 51.58%, #610F96 70.3%, #610F96 70.3%, #57038D 100%)',
+  orangeGradient: 'linear-gradient(146deg, #FAD961 0%, #F76B1C 100%)',
 };
