68d11e8b0dfcb4b27f5b4855743b6dc60259d8ba
Tankred Hase <mail@tankredhase.de>
2018-03-26 16:35:23 +0200

    Add font stories

--- stories/index.js
+++ stories/index.js
@@ -45,6 +45,36 @@ storiesOf('Colors', module)
     <Container style={{ backgroundColor: colors.blueSig }} />
   ));
 
+storiesOf('Fonts', module)
+  .add('OpenSans Light', () => renderFont('OpenSans Light'))
+  .add('OpenSans Regular', () => renderFont('OpenSans Regular'))
+  .add('OpenSans SemiBold', () => renderFont('OpenSans SemiBold'))
+  .add('OpenSans Bold', () => renderFont('OpenSans Bold'))
+  .add('Poppins Thin', () => renderFont('Poppins Thin'))
+  .add('Poppins ExtraLight', () => renderFont('Poppins ExtraLight'))
+  .add('Poppins Light', () => renderFont('Poppins Light'))
+  .add('Poppins Regular', () => renderFont('Poppins Regular'))
+  .add('Poppins Medium', () => renderFont('Poppins Medium'))
+  .add('Poppins SemiBold', () => renderFont('Poppins SemiBold'))
+  .add('Poppins Bold', () => renderFont('Poppins Bold'))
+  .add('WorkSans Thin', () => renderFont('WorkSans Thin'))
+  .add('WorkSans ExtraLight', () => renderFont('WorkSans ExtraLight'))
+  .add('WorkSans Light', () => renderFont('WorkSans Light'))
+  .add('WorkSans Regular', () => renderFont('WorkSans Regular'))
+  .add('WorkSans Medium', () => renderFont('WorkSans Medium'))
+  .add('WorkSans SemiBold', () => renderFont('WorkSans SemiBold'))
+  .add('WorkSans Bold', () => renderFont('WorkSans Bold'));
+
+const renderFont = fontFamily => (
+  <Container style={{ backgroundImage: colors.purpleGradient }}>
+    <MainContent style={{ justifyContent: 'center' }}>
+      <Text style={{ fontFamily }}>
+        The quick brown fox jumps over the lazy dog.
+      </Text>
+    </MainContent>
+  </Container>
+);
+
 storiesOf('Layout', module).add('With Header', () => (
   <Container style={{ backgroundImage: colors.purpleGradient }}>
     <Header
