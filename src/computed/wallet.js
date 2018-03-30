4f788663ecae1124f69b2b46b5c6c68199c552cf
Tankred Hase <mail@tankredhase.de>
2018-03-30 22:13:14 +0200

    Compute wallet channel balance values

--- src/computed/wallet.js
+++ src/computed/wallet.js
@@ -1,15 +1,38 @@
 import { computed, extendObservable } from 'mobx';
-import { formatNumber } from '../helper';
+import { formatNumber, formatFiat } from '../helper';
+import { UNITS } from '../config';
 
 const ComputedWallet = store => {
   extendObservable(store, {
-    computedBalance: computed(() => {
-      const { balanceSatoshis } = store;
-      return formatNumber(balanceSatoshis);
+    balanceUnit: computed(() => {
+      const { balanceSatoshis, settings } = store;
+      const unit = UNITS[settings.unit];
+      return formatNumber(balanceSatoshis / unit.denominator);
     }),
-    computedChannelsBalance: computed(() => {
-      const { channelBalanceSatoshis } = store;
-      return formatNumber(channelBalanceSatoshis);
+    channelBalanceUnit: computed(() => {
+      const { channelBalanceSatoshis, settings } = store;
+      const unit = UNITS[settings.unit];
+      return formatNumber(channelBalanceSatoshis / unit.denominator);
+    }),
+    balanceFiat: computed(() => {
+      const { balanceSatoshis, settings } = store;
+      const rate = settings.exchangeRate[settings.fiat];
+      const balance = balanceSatoshis / rate / UNITS.btc.denominator;
+      return formatFiat(balance, settings.fiat);
+    }),
+    channelBalanceFiat: computed(() => {
+      const { channelBalanceSatoshis, settings } = store;
+      const rate = settings.exchangeRate[settings.fiat];
+      const balance = channelBalanceSatoshis / rate / UNITS.btc.denominator;
+      return formatFiat(balance, settings.fiat);
+    }),
+    unitLabel: computed(() => {
+      const { settings } = store;
+      return !settings.displayFiat ? UNITS[settings.unit].display : null;
+    }),
+    channelBalanceLabel: computed(() => {
+      const { settings, channelBalanceUnit, channelBalanceFiat } = store;
+      return settings.displayFiat ? channelBalanceFiat : channelBalanceUnit;
     }),
   });
 };
--- test/unit/computed/wallet.spec.js
+++ test/unit/computed/wallet.spec.js
@@ -1,29 +1,50 @@
-import { observable, useStrict } from 'mobx';
+import { Store } from '../../../src/store';
 import ComputedWallet from '../../../src/computed/wallet';
 
 describe('Computed Wallet Unit Tests', () => {
   let store;
 
   beforeEach(() => {
-    useStrict(false);
+    store = new Store();
   });
 
   describe('ComputedWallet()', () => {
-    it('should work with empty store', () => {
-      store = observable({});
+    it('should work with initial store', () => {
       ComputedWallet(store);
-      expect(store.computedBalance, 'to equal', '');
-      expect(store.computedChannelsBalance, 'to equal', '');
+      expect(store.balanceUnit, 'to equal', '0');
+      expect(store.channelBalanceUnit, 'to equal', '0');
+      expect(store.balanceFiat, 'to match', /0[,.]0{2}/);
+      expect(store.channelBalanceFiat, 'to match', /0[,.]0{2}/);
+      expect(store.unitLabel, 'to equal', 'bits');
+      expect(store.channelBalanceLabel, 'to equal', '0');
     });
 
-    it('should format balance and channel balance', () => {
-      store = observable({
-        balanceSatoshis: '1000',
-        channelBalanceSatoshis: 10000,
-      });
+    it('should display channel balance in usd', () => {
+      store.settings.displayFiat = true;
+      store.settings.exchangeRate.usd = 0.00014503;
+      store.balanceSatoshis = 100000000;
+      store.channelBalanceSatoshis = 10000;
       ComputedWallet(store);
-      expect(store.computedBalance.length, 'to equal', 5);
-      expect(store.computedChannelsBalance.length, 'to equal', 6);
+      expect(store.balanceUnit, 'to match', /^1[,.]0{3}[,.]0{3}$/);
+      expect(store.channelBalanceUnit, 'to equal', '100');
+      expect(store.balanceFiat, 'to match', /6[,.]895[,.]13/);
+      expect(store.channelBalanceFiat, 'to match', /0[,.]69/);
+      expect(store.unitLabel, 'to equal', null);
+      expect(store.channelBalanceLabel, 'to match', /0[,.]69/);
+    });
+
+    it('should display channel balance in sat', () => {
+      store.settings.exchangeRate.usd = 0.00014503;
+      store.balanceSatoshis = 100000000;
+      store.channelBalanceSatoshis = 10000;
+      store.settings.unit = 'sat';
+      ComputedWallet(store);
+      expect(store.balanceUnit, 'to match', /^1{1}0{2}[,.]0{3}[,.]0{3}$/);
+      expect(store.channelBalanceUnit, 'to match', /^1{1}0{1}[,.]0{3}$/);
+      expect(store.balanceFiat, 'to match', /6[,.]895[,.]13/);
+      expect(store.channelBalanceFiat, 'to match', /0[,.]69/);
+      expect(store.unitLabel, 'to equal', 'SAT');
+      expect(store.channelBalanceLabel, 'to match', /^1{1}0{1}[,.]0{3}$/);
     });
   });
 });
