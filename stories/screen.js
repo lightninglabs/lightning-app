6b430925a50505cb29498367b476b36e1ae69382
Tankred Hase <mail@tankredhase.de>
2018-03-30 22:18:24 +0200

    Set dummy data in home story

--- stories/screen.js
+++ stories/screen.js
@@ -9,3 +9,9 @@ const store = new Store();
 storiesOf('Screens', module)
   .add('Welcome', () => <WelcomeView />)
   .add('Home', () => <HomeView store={store} />);
+
+// set some dummy data
+store.settings.unit = 'bit';
+store.channelBalanceSatoshis = 59876543;
+store.settings.exchangeRate.usd = 0.00014503;
+store.settings.displayFiat = true;
