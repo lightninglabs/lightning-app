d23ab661257a13d6b9208fc34e09766ca423268e
Valentine Wallace <vwallace@protonmail.com>
2018-03-31 18:23:17 -0700

    Add QRCode component

--- stories/screen.js
+++ stories/screen.js
@@ -7,6 +7,7 @@ import HomeView from '../src/view/home';
 import { Store } from '../src/store';
 import WalletAction from '../src/action/wallet';
 import NavAction from '../src/action/nav';
+import PaymentRequest from '../src/view/payment-request';
 
 const store = new Store();
 const wallet = new WalletAction(store);
@@ -15,7 +16,14 @@ const nav = sinon.createStubInstance(NavAction);
 storiesOf('Screens', module)
   .add('Welcome', () => <WelcomeView />)
   .add('Transactions', () => <TransactionView store={store} />)
-  .add('Home', () => <HomeView store={store} wallet={wallet} nav={nav} />);
+  .add('Home', () => <HomeView store={store} wallet={wallet} nav={nav} />)
+  .add('Payment Request', () => (
+    <PaymentRequest
+      amount="456,780"
+      invoice="lnbc4567800n1pdvqx48pp5eng6uyqnkdlx93m2598ug93qtuls8gapygxznshzd56h7n5cxs0sdp9gehhygr5dpjjqmr0wejjqmmxyp3xjarrda5kucqzysmhyrleqpt3yqf5nctzsr3hvrv9vhhnawazkwyzu8t4mf85tllsyjsf8hgu5nt6dj3jaljjgmt999xnlsweqvatypzlu34nhpjlxf59qp4dn2pv"
+      message="For the love of bitcoin"
+    />
+));
 
 // set some dummy data
 store.balanceSatoshis = 798765432;
