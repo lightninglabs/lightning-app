a19386918a577541b1ddd46a29bb7ecc899d26ce
Tankred Hase <mail@tankredhase.de>
2018-04-02 19:05:31 +0200

    Add QR code separator below header to home screen

--- src/view/home.js
+++ src/view/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 import Background from '../component/background';
 import MainContent from '../component/main-content';
 import { BalanceLabel } from '../component/label';
 import { Header, Title } from '../component/header';
+import { QrButton } from '../component/button';
+import { colors } from '../component/style';
 
 const styles = StyleSheet.create({
   content: {
@@ -17,9 +19,10 @@ const Home = ({ store }) => {
   const { channelBalanceLabel, unitLabel } = store;
   return (
     <Background image="purple-gradient-bg">
-      <Header separator>
+      <Header>
         <Title title="Wallet" />
       </Header>
+      <QrCodeSeparator goFundWallet={() => {}} />
       <MainContent style={styles.content}>
         <BalanceLabel unit={unitLabel}>{channelBalanceLabel}</BalanceLabel>
       </MainContent>
@@ -29,6 +32,42 @@ const Home = ({ store }) => {
 
 Home.propTypes = {
   store: PropTypes.object.isRequired,
+};
+
+//
+// QR Code Separator
+//
+
+const qrStyles = StyleSheet.create({
+  wrapper: {
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    alignItems: 'center',
+    top: -20,
+  },
+  button: {
+    width: 42,
+  },
+  separator: {
+    flex: 1,
+    height: 1,
+    boxShadow: `0 0.25px ${colors.white}`,
+    marginBottom: 21,
+  },
+});
+
+const QrCodeSeparator = ({ goFundWallet }) => (
+  <View style={qrStyles.wrapper}>
+    <View style={qrStyles.separator} />
+    <QrButton image="qr" onPress={goFundWallet} style={qrStyles.button}>
+      Add coin
+    </QrButton>
+    <View style={qrStyles.separator} />
+  </View>
+);
+
+QrCodeSeparator.propTypes = {
+  goFundWallet: PropTypes.func.isRequired,
 };
 
 export default observer(Home);
