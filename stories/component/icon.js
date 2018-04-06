283ec66ced32cba34525a8f295c08ee6394e8d14
Tankred Hase <mail@tankredhase.de>
2018-04-06 17:39:36 +0200

    Document missing icons in storybook

--- stories/component/icon.js
+++ stories/component/icon.js
@@ -13,4 +13,14 @@ storiesOf('Icons', module)
   .add('Back', () => <Icon image="back" style={{ height: 20, width: 12 }} />)
   .add('Cancel', () => (
     <Icon image="cancel" style={{ height: 21, width: 21 }} />
+  ))
+  .add('Arrow Down', () => (
+    <Icon image="arrow-down" style={{ height: 9, width: 23 }} />
+  ))
+  .add('Lightning Bolt', () => (
+    <Icon image="lightning-bolt" style={{ height: 126, width: 64 }} />
+  ))
+  .add('QR Code', () => <Icon image="qr" style={{ height: 39, width: 40 }} />)
+  .add('Settings', () => (
+    <Icon image="settings" style={{ height: 21, width: 20 }} />
   ));
