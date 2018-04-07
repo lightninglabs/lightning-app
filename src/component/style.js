8961daa80b26caeda0218021e86e5a4ddc6001c1
Tankred Hase <mail@tankredhase.de>
2018-04-07 23:45:16 +0200

    Add greyBorder color

--- src/component/style.js
+++ src/component/style.js
@@ -3,6 +3,7 @@ export const colors = {
   whiteBg: '#F5F5F5',
   grey: '#C5C8CF',
   greyText: '#848A99',
+  greyBorder: '#6B6969',
   purple: '#57038D',
   orange: '#F66B1C',
   blackDark: '#252F4A',
