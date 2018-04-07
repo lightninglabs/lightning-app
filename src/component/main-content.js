86604b8efc14e5e322ec77b427abed19b59395a3
Tankred Hase <mail@tankredhase.de>
2018-04-07 14:43:18 +0200

    Wrap MainContent in ScrollView to make it scrollable

--- src/component/main-content.js
+++ src/component/main-content.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { View, ViewPropTypes, StyleSheet } from 'react-native';
+import { View, ScrollView, ViewPropTypes, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
-  content: {
+  scroll: {
     flex: 1,
+  },
+  content: {
+    flexGrow: 1,
     alignItems: 'center',
   },
 });
 
 const MainContent = ({ children, style }) => (
-  <View style={[styles.content, style]}>{children}</View>
+  <ScrollView contentContainerStyle={styles.scroll}>
+    <View style={[styles.content, style]}>{children}</View>
+  </ScrollView>
 );
 
 MainContent.propTypes = {
