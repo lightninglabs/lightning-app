b431bfbc63253681981fc82dabe19d15f0968656
Tankred Hase <mail@tankredhase.de>
2018-04-07 14:44:48 +0200

    Add scrollable MainContent example in Card Form layout story

--- stories/layout.js
+++ stories/layout.js
@@ -34,12 +34,18 @@ storiesOf('Layout', module)
         </Title>
         <CancelButton onPress={action('cancel')} />
       </Header>
-      <Card>
-        <Text style={{ color: colors.blackDark }}>
-          Here are some instructions...
-        </Text>
-        <MainContent />
-        <PillButton onPress={action('clicked')}>Next</PillButton>
-      </Card>
+      <MainContent>
+        <Card>
+          <Text style={{ color: colors.blackText, paddingBottom: 40 }}>
+            {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non ultricies est. Ut ut egestas tellus. Duis hendrerit accumsan turpis, nec lacinia leo pharetra sed. Praesent finibus volutpat velit. Vestibulum eget ultrices orci. Aenean iaculis porta pretium. Maecenas placerat, nibh id ultricies hendrerit, arcu turpis bibendum metus, eget pharetra urna dui quis risus. Vivamus rhoncus interdum massa eu ornare. Integer egestas metus ut mi mollis venenatis. Vestibulum non ultrices velit. Nulla dapibus purus id egestas pellentesque. Proin pretium elit mattis neque hendrerit, sed placerat arcu ultrices. Aliquam erat volutpat.
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices aliquet mauris nec pulvinar. Suspendisse ornare, ligula in elementum egestas, eros dolor porta leo, sed iaculis massa augue vitae quam. Pellentesque at nulla maximus, molestie odio bibendum, eleifend justo. Vestibulum vel interdum nisi, et blandit dolor. Praesent mattis sapien in orci lobortis, in dapibus sem porta. Etiam imperdiet massa in felis sollicitudin vulputate. Nullam in dignissim mauris. Etiam consequat tortor in dictum mollis. Ut nec egestas nunc. Cras ex velit, pellentesque in ligula sed, bibendum laoreet mauris. Integer sit amet ligula vitae purus sodales porta.
+
+            Quisque nec laoreet nulla. Maecenas ullamcorper velit est. Phasellus efficitur, nisi in sodales tincidunt, ex ligula dictum dolor, sit amet eleifend sapien velit eget quam. Praesent dolor nisi, dignissim a nunc egestas, elementum scelerisque velit. Donec at turpis eros. Pellentesque id laoreet nibh. Proin et nulla lorem. Vestibulum fringilla auctor augue, ut imperdiet lectus dictum vel. Nullam at magna massa. Suspendisse vitae ipsum nec mauris pellentesque ullamcorper. Curabitur semper suscipit pharetra. Vestibulum id bibendum tellus.`}
+          </Text>
+          <MainContent />
+          <PillButton onPress={action('clicked')}>Next</PillButton>
+        </Card>
+      </MainContent>
     </Background>
   ));
