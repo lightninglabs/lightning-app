eebb95d252483ddad57698d72ac92610f6e36c39
Tankred Hase <mail@tankredhase.de>
2018-04-10 14:57:20 +0200

    Wrap Card component in MainContent in story

--- stories/component/card.js
+++ stories/component/card.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Background from '../../src/component/background';
+import MainContent from '../../src/component/main-content';
 import Card from '../../src/component/card';
 
 storiesOf('Card', module)
   .addDecorator(story => (
-    <Background image="purple-gradient-bg">{story()}</Background>
+    <Background image="purple-gradient-bg">
+      <MainContent>{story()}</MainContent>
+    </Background>
   ))
   .add('Plain', () => <Card />);
