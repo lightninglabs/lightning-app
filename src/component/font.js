8a2e24e36a0123e309ab4570f1fa297bcab36d21
Tankred Hase <mail@tankredhase.de>
2018-04-11 13:29:14 +0200

    Move font asset imports to font.js module

---
+++ src/component/font.js
@@ -0,0 +1,34 @@
+/**
+ * @fileOverview this module imports font assets for
+ * react-native-web using webpack. For iOS and Android
+ * we'll need to create platform specific font.ios.js
+ * and font.android.js modules.
+ */
+
+import '../../assets/font/OpenSans-Light.ttf';
+import '../../assets/font/OpenSans-Regular.ttf';
+import '../../assets/font/OpenSans-SemiBold.ttf';
+import '../../assets/font/OpenSans-Bold.ttf';
+import '../../assets/font/OpenSans-ExtraBold.ttf';
+
+import '../../assets/font/Poppins-Thin.ttf';
+import '../../assets/font/Poppins-ExtraLight.ttf';
+import '../../assets/font/Poppins-Light.ttf';
+import '../../assets/font/Poppins-Regular.ttf';
+import '../../assets/font/Poppins-Medium.ttf';
+import '../../assets/font/Poppins-SemiBold.ttf';
+import '../../assets/font/Poppins-Bold.ttf';
+import '../../assets/font/Poppins-ExtraBold.ttf';
+import '../../assets/font/Poppins-Black.ttf';
+
+import '../../assets/font/WorkSans-Thin.ttf';
+import '../../assets/font/WorkSans-ExtraLight.ttf';
+import '../../assets/font/WorkSans-Light.ttf';
+import '../../assets/font/WorkSans-Regular.ttf';
+import '../../assets/font/WorkSans-Medium.ttf';
+import '../../assets/font/WorkSans-SemiBold.ttf';
+import '../../assets/font/WorkSans-Bold.ttf';
+import '../../assets/font/WorkSans-ExtraBold.ttf';
+import '../../assets/font/WorkSans-Black.ttf';
+
+import './font.css';
