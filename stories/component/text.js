a59da1118a9fa8d4da62ff72712b957a279c3174
Tankred Hase <mail@tankredhase.de>
2018-04-12 13:35:30 +0200

    Add Header components to text stories

--- stories/component/text.js
+++ stories/component/text.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Background from '../../src/component/background';
 import MainContent from '../../src/component/main-content';
-import { Text, CopyText, H1Text } from '../../src/component/text';
+import {
+  Text,
+  CopyText,
+  H1Text,
+  H3Text,
+  H4Text,
+} from '../../src/component/text';
 
 storiesOf('Text', module)
   .addDecorator(story => (
@@ -16,4 +22,6 @@ storiesOf('Text', module)
   .add('Copy Text', () => (
     <CopyText>The quick brown fox jumps over the lazy dog.</CopyText>
   ))
-  .add('Heading 1', () => <H1Text>Heading 1</H1Text>);
+  .add('Heading 1', () => <H1Text>Heading 1</H1Text>)
+  .add('Heading 3', () => <H3Text>Heading 3</H3Text>)
+  .add('Heading 4', () => <H4Text>Heading 4</H4Text>);
