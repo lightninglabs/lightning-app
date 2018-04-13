d051f3390e847a95434749ee4f4c2e592b064082
Tankred Hase <mail@tankredhase.de>
2018-04-13 13:34:47 +0200

    Add MainView for new design

---
+++ src/view/main.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import Container from '../component/container';
+import Home from './home';
+import Deposit from './deposit';
+import Transaction from './transaction';
+import { wallet, nav } from '../action';
+import store from '../store';
+
+class MainView extends Component {
+  render() {
+    const { route } = store;
+    return (
+      <Container>
+        {route === 'Home' && <Home store={store} wallet={wallet} nav={nav} />}
+        {route === 'FundWallet' && <Deposit store={store} nav={nav} />}
+        {route === 'Transactions' && <Transaction store={store} nav={nav} />}
+      </Container>
+    );
+  }
+}
+
+export default observer(MainView);
