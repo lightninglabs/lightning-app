c7f18d9d28934b6cd0628e0ed616c1071521422b
Tankred Hase <mail@tankredhase.de>
2018-04-17 14:34:30 +0200

    Add field stories

--- stories/component/field.js
+++ stories/component/field.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { NamedField } from '../../src/component/field';
+import {
+  InputField,
+  NamedField,
+  AmountInputField,
+} from '../../src/component/field';
 
-storiesOf('Field', module).add('Named Field', () => (
-  <NamedField name="Label">Named field</NamedField>
-));
+storiesOf('Field', module)
+  .add('Input Field', () => (
+    <InputField placeholder="Input Field" onChangeText={text => {}} />
+  ))
+  .add('Named Field', () => <NamedField name="Label">Named field</NamedField>)
+  .add('Amount Input Field', () => (
+    <AmountInputField onChangeText={text => {}} />
+  ));
