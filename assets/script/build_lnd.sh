b49c699387c2a63b5e882905d6cb080d784c9675
Tankred Hase <mail@tankredhase.de>
2018-06-03 14:04:34 +0200

    Remove unnecessary go builds during deploy

--- assets/script/build_lnd.sh
+++ assets/script/build_lnd.sh
@@ -5,21 +5,14 @@ GOPATH=$HOME/gocode
 GOROOT=$HOME/go
 PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
-# copy over lnd and btcd binaries for linux
-cp $GOPATH/bin/lnd $GOPATH/bin/btcd ./assets/bin/linux
-
-# build binaries for darwin/windows
-cd ./assets/bin/darwin
-env GOOS="darwin" GOARCH="amd64" go build -v github.com/lightningnetwork/lnd
-env GOOS="darwin" GOARCH="amd64" go build -v github.com/btcsuite/btcd
-cd ../win32
-env GOOS="windows" GOARCH="386" go build -v github.com/lightningnetwork/lnd
-env GOOS="windows" GOARCH="386" go build -v github.com/btcsuite/btcd
-
-# build the packages using electron-builder on docker
-cd $TRAVIS_BUILD_DIR
-
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  # build binaries forwindows
+  cd ../win32
+  env GOOS="windows" GOARCH="386" go build -v github.com/lightningnetwork/lnd
+  env GOOS="windows" GOARCH="386" go build -v github.com/btcsuite/btcd
+
+  # build the packages using electron-builder on docker
+  cd $TRAVIS_BUILD_DIR
   env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_' > env.txt
   docker run --rm \
     --env-file env.txt \
