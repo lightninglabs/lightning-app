a2d2279ba2a4648c8a06e6a168b90902f165ce3c
Tankred Hase <mail@tankredhase.de>
2018-06-04 14:48:05 +0200

    Revert "Set linux docker env like in documentation"
    This reverts commit 0b502d14776ef8ddb056e05d8651a85c0ce26961.

--- assets/script/build_app.sh
+++ assets/script/build_app.sh
@@ -15,8 +15,9 @@ else
 
   # build the packages using electron-builder on docker
   cd $TRAVIS_BUILD_DIR
-  docker run --rm -ti \
-    --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') \
+  env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_' > env.txt
+  docker run --rm \
+    --env-file env.txt \
     --env ELECTRON_CACHE="/root/.cache/electron" \
     --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" \
     -v ${PWD}:/project \
@@ -25,4 +26,5 @@ else
     -v ~/.cache/electron-builder:/root/.cache/electron-builder \
     electronuserland/builder:wine \
     /bin/bash -c "npm i && npm run electron-pack -- --linux --win"
+  rm env.txt
 fi
