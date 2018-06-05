a60b383f1908f26cfb231ca8d8df3135740d14a2
Tankred Hase <mail@tankredhase.de>
2018-06-05 16:42:35 +0200

    Change back to binName

--- public/lnd-child-process.js
+++ public/lnd-child-process.js
@@ -7,7 +7,7 @@ function getProcessName(binName) {
   const filePath = __dirname.includes('asar')
     ? path.join(__dirname, '..', '..', 'assets', 'bin', os.platform(), filename)
     : path.join(__dirname, '..', 'assets', 'bin', os.platform(), filename);
-  return cp.spawnSync('type', [filePath]).status === 0 ? filePath : filename;
+  return cp.spawnSync('type', [filePath]).status === 0 ? filePath : binName;
 }
 
 async function startChildProcess(name, args, logger) {
