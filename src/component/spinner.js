32f4d7a942d8580c9e8863ecc11dd24241743c9f
Valentine Wallace <vwallace@protonmail.com>
2018-06-13 20:07:59 -0700

    V1 of timed spinner

--- src/component/spinner.js
+++ src/component/spinner.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { ActivityIndicator, StyleSheet } from 'react-native';
+import {
+  ActivityIndicator,
+  StyleSheet,
+  View,
+  ART,
+} from 'react-native';
+import PropTypes from 'prop-types';
 import { color } from './style';
 
 //
@@ -19,3 +25,63 @@ export const SmallSpinner = ({ ...props }) => (
     {...props}
   />
 );
+
+//
+// Timed Spinner
+//
+const { Surface, Shape, Path, Group } = ART;
+const width = 10;
+const linecap = 'butt';
+const spinnerColor = color.purple;
+
+export const TimedSpinner = ({ size, percent }) => (
+  <View>
+    <Surface width={size} height={size}>
+      <Group rotation={0} originX={size / 2} originY={size / 2}>
+        <Shape d={calcCirclePath(size, percent)}
+               stroke={spinnerColor}
+               strokeWidth={width}
+               strokeCap={linecap}/>
+      </Group>
+    </Surface>
+  </View>
+);
+
+TimedSpinner.propTypes = {
+  size: PropTypes.number,
+  percent: PropTypes.number.isRequired,
+};
+
+const calcCirclePath = (size, percent) => {
+  const fill = extractFill(percent);
+  return getCirclePath(
+    size / 2,
+    size / 2,
+    size / 2 - width / 2,
+    0,
+    360 * .9999 * fill / 100
+  );
+};
+
+const getCirclePath = (cx, cy, r, startDegree, endDegree) => {
+  let endDegreeInRadians = endDegree * Math.PI / 180;
+  let p = Path();
+  p.move(cx + r, cy);
+  p.arc(
+    -r + r * Math.cos(endDegreeInRadians),
+    r * Math.sin(endDegreeInRadians),
+    r,
+    r,
+    endDegree >= 180
+  );
+  return p;
+};
+
+const extractFill = fill => {
+  if (fill < 0.01) {
+    return 0;
+  } else if (fill > 100) {
+    return 100;
+  }
+  return fill;
+};
--- stories/component/spinner.js
+++ stories/component/spinner.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import MainContent from '../../src/component/main-content';
-import { SmallSpinner } from '../../src/component/spinner';
+import { TimedSpinner, SmallSpinner } from '../../src/component/spinner';
 
 storiesOf('Spinner', module)
   .addDecorator(story => (
     <MainContent style={{ justifyContent: 'center' }}>{story()}</MainContent>
   ))
-  .add('SmallSpinner', () => <SmallSpinner />);
+  .add('SmallSpinner', () => <SmallSpinner />)
+  .add('TimedSpinner', () => <TimedSpinner size={120} percent={90} />);
