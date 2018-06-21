8f2fcfa99bb1257f42ef668def10ecea56fa466e
Valentine Wallace <vwallace@protonmail.com>
2018-06-20 17:37:49 -0700

    Implement spinner for loading network using svgs

--- src/component/spinner.js
+++ src/component/spinner.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import {
-  ActivityIndicator,
-  StyleSheet,
-  View,
-  ART,
-} from 'react-native';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import PropTypes from 'prop-types';
-import { color } from './style';
+import { color, font } from './style';
+import Icon from '../component/icon';
+import Text from '../component/text';
+import Svg, { Path, Circle, Defs, Stop, LinearGradient } from 'svgs';
 
 //
 // Small Spinner
@@ -27,61 +25,104 @@ export const SmallSpinner = ({ ...props }) => (
 );
 
 //
-// Timed Spinner
+// Load Network Spinner
 //
-const { Surface, Shape, Path, Group } = ART;
-const width = 10;
-const linecap = 'butt';
-const spinnerColor = color.purple;
 
-export const TimedSpinner = ({ size, percent }) => (
-  <View>
-    <Surface width={size} height={size}>
-      <Group rotation={0} originX={size / 2} originY={size / 2}>
-        <Shape d={calcCirclePath(size, percent)}
-               stroke={spinnerColor}
-               strokeWidth={width}
-               strokeCap={linecap}/>
-      </Group>
-    </Surface>
+const size = 121;
+const progressWidth = 5;
+
+const loadNetworkStyles = StyleSheet.create({
+  spinner: {
+    margin: 20,
+  },
+  fill: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bolt: {
+    height: 32,
+    width: 16,
+  },
+  copy: {
+    fontSize: font.sizeS,
+    marginTop: 10,
+    color: color.white,
+    textAlign: 'center',
+  },
+});
+
+export const LoadNetworkSpinner = ({ percentage, msg }) => (
+  <View style={loadNetworkStyles.spinner}>
+    <View style={{ width: size, height: size }}>
+      <Svg width={size} height={size}>
+        <Defs>
+          <LinearGradient id="linearGrad" x1="0" y1="0" x2="1" y2="1">
+            <Stop offset="0%" stopColor={color.spinnerLightPurple} />
+            <Stop offset="50%" stopColor={color.spinnerMedPurple} />
+            <Stop offset="70%" stopColor={color.spinnerMedDarkPurple} />
+            <Stop offset="100%" stopColor={color.purple} />
+          </LinearGradient>
+        </Defs>
+        <Circle
+          cx={size / 2}
+          cy={size / 2}
+          r={size / 2}
+          fill={color.blackDark}
+        />
+        <Path
+          d={`M${size / 2} ${size / 2} L${size / 2} 0 ${generateArc(
+            percentage,
+            size / 2
+          )} Z`}
+          fill="url(#linearGrad)"
+        />
+        {
+          <Circle
+            cx={size / 2}
+            cy={size / 2}
+            r={size / 2 - progressWidth}
+            fill={color.blackDark}
+          />
+        }
+      </Svg>
+      <View style={loadNetworkStyles.fill}>
+        <Icon image="lightning-bolt" style={loadNetworkStyles.bolt} />
+      </View>
+    </View>
+    <Text style={loadNetworkStyles.copy}>{msg}</Text>
   </View>
 );
 
-TimedSpinner.propTypes = {
-  size: PropTypes.number,
-  percent: PropTypes.number.isRequired,
+LoadNetworkSpinner.propTypes = {
+  percentage: PropTypes.number.isRequired,
+  msg: PropTypes.string.isRequired,
 };
 
-const calcCirclePath = (size, percent) => {
-  const fill = extractFill(percent);
-  return getCirclePath(
-    size / 2,
-    size / 2,
-    size / 2 - width / 2,
-    0,
-    360 * .9999 * fill / 100
-  );
-};
-
-const getCirclePath = (cx, cy, r, startDegree, endDegree) => {
-  let endDegreeInRadians = endDegree * Math.PI / 180;
-  let p = Path();
-  p.move(cx + r, cy);
-  p.arc(
-    -r + r * Math.cos(endDegreeInRadians),
-    r * Math.sin(endDegreeInRadians),
-    r,
-    r,
-    endDegree >= 180
-  );
-  return p;
-};
-
-const extractFill = fill => {
-  if (fill < 0.01) {
-    return 0;
-  } else if (fill > 100) {
-    return 100;
+const generateArc = (percentage, radius) => {
+  if (percentage === 0) {
+    percentage = 1;
+  } else if (percentage === 100) {
+    percentage = 99.999;
   }
-  return fill;
+  const a = percentage * 2 * Math.PI / 100; // angle (in radian) depends on percentage
+  const r = radius; // radius of the circle
+  var rx = r,
+    ry = r,
+    xAxisRotation = 0,
+    largeArcFlag = 1,
+    sweepFlag = 1,
+    x = r + r * Math.sin(a),
+    y = r - r * Math.cos(a);
+  if (percentage <= 50) {
+    largeArcFlag = 0;
+  } else {
+    largeArcFlag = 1;
+  }
+
+  return `A${rx} ${ry} ${xAxisRotation} ${largeArcFlag} ${sweepFlag} ${x} ${y}`;
 };
--- stories/component/spinner.js
+++ stories/component/spinner.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import MainContent from '../../src/component/main-content';
-import { TimedSpinner, SmallSpinner } from '../../src/component/spinner';
+import { LoadNetworkSpinner, SmallSpinner } from '../../src/component/spinner';
+import { color } from '../../src/component/style';
+import Background from '../../src/component/background';
 
 storiesOf('Spinner', module)
   .addDecorator(story => (
     <MainContent style={{ justifyContent: 'center' }}>{story()}</MainContent>
   ))
   .add('SmallSpinner', () => <SmallSpinner />)
-  .add('TimedSpinner', () => <TimedSpinner size={120} percent={90} />);
+  .add('LoadNetworkSpinner', () => (
+    <Background
+      color={color.blackDark}
+      style={{ flexDirection: 'row', width: '100%' }}
+    >
+      <LoadNetworkSpinner percentage={30} msg="Loading network..." />
+      <LoadNetworkSpinner percentage={50} msg="Almost done..." />
+      <LoadNetworkSpinner percentage={100} msg="Just a few seconds..." />
+    </Background>
+  ));
