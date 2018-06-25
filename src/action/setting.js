96c689fb3260bfea9af2d4babda26dde46acd38a
Valentine Wallace <vwallace@protonmail.com>
2018-06-25 15:36:05 -0700

    Add init func and error msg
    Throw an error if the requested unit is not btc, bit or sat

--- src/action/setting.js
+++ src/action/setting.js
@@ -1,19 +1,21 @@
-import { UNITS } from '../config';
+import { UNITS, DEFAULT_UNIT } from '../config';
 
 class SettingAction {
   constructor(store) {
     this._store = store;
   }
 
+  init() {
+    this._store.settings.unit = DEFAULT_UNIT;
+  }
+
   //
-  // Bitcoin Unit actions
+  // Bitcoin unit setting actions
   //
 
-  setBitcoinUnit(unit) {
-    if (this._store.settings.unit === unit) {
-      return;
-    } else if (!(unit in UNITS)) {
-      return;
+  setBitcoinUnit({ unit }) {
+    if (!(unit in UNITS)) {
+      throw new Error(`Invalid bitcoin unit: ${unit}`);
     }
     this._store.settings.unit = unit;
   }
