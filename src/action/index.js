1b9b11a32fcdac797b3352d7b159e79b55d2dd8d
Tankred Hase <mail@tankredhase.de>
2018-08-02 15:32:51 +0200

    Add docs to main action module

--- src/action/index.js
+++ src/action/index.js
@@ -1,3 +1,9 @@
+/**
+ * @fileOverview this is the main action module where all actions are initilized
+ * by injecting dependencies and then triggering startup actions when certain
+ * flags on the global store are set to true.
+ */
+
 import { observe } from 'mobx';
 import { AsyncStorage, Clipboard } from 'react-native';
 import { nap } from '../helper';
@@ -21,7 +27,7 @@ const ipcRenderer = window.ipcRenderer; // exposed to sandbox via preload.js
 // Inject dependencies
 //
 
-store.init();
+store.init(); // initialize computed values
 
 export const db = new AppStorage(store, AsyncStorage);
 export const log = new LogAction(store, ipcRenderer);
@@ -43,28 +49,43 @@ export const invoice = new InvoiceAction(
 export const payment = new PaymentAction(store, grpc, transaction, nav, notify);
 export const setting = new SettingAction(store, wallet, db);
 
-payment.listenForUrl(ipcRenderer);
+payment.listenForUrl(ipcRenderer); // enable incoming url handler
 
 //
 // Init actions
 //
 
-db.restore();
+db.restore(); // read user settings from disk
 
+/**
+ * Triggered after user settings are restored from disk.
+ */
 observe(store, 'loaded', async () => {
   await grpc.initUnlocker();
 });
 
+/**
+ * Triggered after the wallet unlocker grpc client is initialized.
+ */
 observe(store, 'unlockerReady', async () => {
   await wallet.init();
 });
 
+/**
+ * Triggered after the user's password has unlocked the wallet.
+ */
 observe(store, 'walletUnlocked', async () => {
   await nap();
   await grpc.closeUnlocker();
   await grpc.initLnd();
 });
 
+/**
+ * Triggered once the main lnd grpc client is inialized. This is when
+ * the user can really begin to interact with the application and calls
+ * to and from lnd can be done. The display the current state of the
+ * lnd node all balances, channels and transactions are fetched.
+ */
 observe(store, 'lndReady', () => {
   info.getInfo();
   wallet.update();
