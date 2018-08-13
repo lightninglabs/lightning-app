84e6c07d9b6a7203776913b7def9de86631b9f67
Tankred Hase <mail@tankredhase.de>
2018-08-13 18:35:03 +0200

    Test inline svg icon buttons on mobile

--- mobile/rn-cli.config.js
+++ mobile/rn-cli.config.js
@@ -5,6 +5,10 @@ module.exports = {
     react: path.resolve(__dirname, 'node_modules/react'),
     'react-native': path.resolve(__dirname, 'node_modules/react-native'),
     'prop-types': path.resolve(__dirname, 'node_modules/prop-types'),
+    'react-native-svg': path.resolve(
+      __dirname,
+      'node_modules/react-native-svg'
+    ),
   },
   getProjectRoots: () => [
     path.resolve(__dirname),
