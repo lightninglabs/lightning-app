07dd4d187112694a2e67559f56a41a1c353b45bd
Tankred Hase <mail@tankredhase.de>
2018-08-16 09:34:02 +0200

    Test background component on iOS with SVG background

--- mobile/App.js
+++ mobile/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import FontLoader from './component/font-loader';
+import Container from '../src/component/container';
+import Background from '../src/component/background';
+import OrangeGradientBg from '../src/asset/img/orange-gradient-bg';
 import { Header, Title } from '../src/component/header';
+import BitcoinIcon from '../src/asset/icon/bitcoin';
 import { CancelButton, BackButton } from '../src/component/button';
 import { color } from '../src/component/style';
 
@@ -15,22 +19,16 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 20,
-  },
-  text: {
-    color: color.black,
-  },
-});
-
 const TestView = () => (
-  <View style={styles.container}>
-    <Header shadow color={color.orange}>
-      <BackButton onPress={() => {}} />
-      <Title title="On-Chain Payment" />
-      <CancelButton onPress={() => {}} />
-    </Header>
-  </View>
+  <Container>
+    <Background node={<OrangeGradientBg style={StyleSheet.absoluteFill} />}>
+      <Header shadow color={color.orange}>
+        <BackButton onPress={() => {}} />
+        <Title title="On-Chain Payment">
+          <BitcoinIcon height={13.6} width={10.8} />
+        </Title>
+        <CancelButton onPress={() => {}} />
+      </Header>
+    </Background>
+  </Container>
 );
