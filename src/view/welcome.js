bbaf5b65075e64ed3f17c1f7f94ccdfc8ea386d3
Tankred Hase <mail@tankredhase.de>
2018-08-17 16:04:36 +0200

    Fix WelcomeView to use Background correctly

--- src/view/welcome.js
+++ src/view/welcome.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Background from '../component/background';
-import MainContent from '../component/main-content';
 import BoltIcon from '../asset/icon/lightning-bolt';
 import LightningWord from '../asset/icon/lightning-word';
 import Text from '../component/text';
 
 const styles = StyleSheet.create({
-  content: {
+  background: {
     justifyContent: 'center',
+    alignItems: 'center',
   },
   boltWrapper: {
     marginTop: 20,
   },
   wordWrapper: {
-    marginTop: 18,
+    marginTop: 20,
   },
   subtitle: {
     marginTop: 12,
@@ -23,16 +23,14 @@ const styles = StyleSheet.create({
 });
 
 const WelcomeView = () => (
-  <Background image="textured-bg">
-    <MainContent style={styles.content}>
-      <View style={styles.boltWrapper}>
-        <BoltIcon height={64 * 1.3} width={126 * 1.3} />
-      </View>
-      <View style={styles.wordWrapper}>
-        <LightningWord height={31.2} width={245.7} />
-      </View>
-      <Text style={styles.subtitle}>By Lightning Labs, INC</Text>
-    </MainContent>
+  <Background image="textured-bg" style={styles.background}>
+    <View style={styles.boltWrapper}>
+      <BoltIcon height={64 * 1.3} width={126 * 1.3} />
+    </View>
+    <View style={styles.wordWrapper}>
+      <LightningWord height={31.2} width={245.7} />
+    </View>
+    <Text style={styles.subtitle}>By Lightning Labs, INC</Text>
   </Background>
 );
 
