30c5851efa0451f17d7406df45b4642c9f2f8f88
Tankred Hase <mail@tankredhase.de>
2018-08-17 16:06:52 +0200

    Test WelcomeScreen on mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import FontLoader from './component/font-loader';
-import Container from '../src/component/container';
-import Background from '../src/component/background';
-import { Header, Title } from '../src/component/header';
-import BitcoinIcon from '../src/asset/icon/bitcoin';
-import { CancelButton, BackButton } from '../src/component/button';
-import { color } from '../src/component/style';
+import Welcome from '../src/view/welcome';
 
 export default class App extends React.Component {
   render() {
     return (
       <FontLoader>
-        <TestView />
+        <Welcome />
       </FontLoader>
     );
   }
 }
-
-const TestView = () => (
-  <Container>
-    <Background image="orange-gradient-bg">
-      <Header shadow color={color.orange}>
-        <BackButton onPress={() => {}} />
-        <Title title="On-Chain Payment">
-          <BitcoinIcon height={13.6} width={10.8} />
-        </Title>
-        <CancelButton onPress={() => {}} />
-      </Header>
-    </Background>
-  </Container>
-);
