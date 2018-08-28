2d251f43944dea510617092f6a80bfcbc1775d2d
Tankred Hase <mail@tankredhase.de>
2018-08-28 12:10:13 +0200

    Remove transaction from the channel action since wallet balances are polled and transaction list is updated when navigating there

--- stories/screen-story.js
+++ stories/screen-story.js
@@ -73,7 +73,7 @@ const payment = new PaymentAction(store, grpc, transaction, nav, notify);
 sinon.stub(payment, 'checkType');
 sinon.stub(payment, 'payBitcoin');
 sinon.stub(payment, 'payLightning');
-const channel = new ChannelAction(store, grpc, transaction, nav, notify);
+const channel = new ChannelAction(store, grpc, nav, notify);
 sinon.stub(channel, 'update');
 sinon.stub(channel, 'connectAndOpen');
 sinon.stub(channel, 'closeSelectedChannel');
