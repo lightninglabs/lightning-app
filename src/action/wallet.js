5b04c6e8f471717a9e4cd94069758d15eb5a9533
Tankred Hase <mail@tankredhase.de>
2018-08-29 09:26:19 +0200

    Revert "Cleanup wallet action"
    This reverts commit 9ba05bdabcc33dc768dad5caad23669364da3fb5.

--- src/action/wallet.js
+++ src/action/wallet.js
@@ -3,7 +3,7 @@
  * call the corresponding GRPC apis for updating wallet balances.
  */
 
-import { observe } from 'mobx';
+import { observe, when } from 'mobx';
 import { toBuffer, parseSat, checkHttpStatus, nap, poll } from '../helper';
 import { MIN_PASSWORD_LENGTH, NOTIFICATION_DELAY, RATE_DELAY } from '../config';
 import * as log from './log';
@@ -277,7 +277,10 @@ class WalletAction {
       this._nav.goNewAddress();
     } else {
       this._nav.goWait();
-      observe(this._store, 'walletAddress', () => this._nav.goNewAddress());
+      when(
+        () => typeof this._store.walletAddress === 'string',
+        () => this._nav.goNewAddress()
+      );
     }
   }
 
