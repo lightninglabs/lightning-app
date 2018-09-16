65b5403f52825884f846b4f61b4e105ed99f3b6d
Karl Ranna <karl@karlranna.com>
2018-09-16 16:26:39 +0300

    Open lightning protocol links on Windows
    Add additional logic to catch the protocol links on Windows since it
    doesn't trigger the `open-url` callback.

--- public/electron.js
+++ public/electron.js
@@ -204,7 +204,11 @@ app.on('quit', () => {
 });
 
 // Prevent multiple instances of the application.
-const duplicateInstance = app.makeSingleInstance(() => {
+const duplicateInstance = app.makeSingleInstance(args => {
+  if (process.platform == 'win32') {
+    const protocolUrl = args && args.slice(1)[0];
+    if (protocolUrl) onOpenUrl(null, protocolUrl);
+  }
   if (win) {
     if (win.isMinimized()) win.restore();
     win.focus();
@@ -213,12 +217,13 @@ const duplicateInstance = app.makeSingleInstance(() => {
 if (duplicateInstance) app.quit();
 
 app.setAsDefaultProtocolClient(PREFIX_NAME);
-app.on('open-url', async (event, url) => {
+const onOpenUrl = async (event, url) => {
   while (!win) {
     await new Promise(resolve => setTimeout(resolve, LND_INIT_DELAY));
   }
   win && win.webContents.send('open-url', url);
-});
+};
+app.on('open-url', onOpenUrl);
 
 process.on('uncaughtException', error => {
   Logger.error('Caught Main Process Error:', error);
