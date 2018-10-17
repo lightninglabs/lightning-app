a09a99905d9756197f9893abb6630fe421d944d0
Tankred Hase <mail@tankredhase.de>
2018-10-17 18:11:43 +0200

    Get navigator demo running

--- mobile/App.js
+++ mobile/App.js
@@ -1,3 +1,102 @@
-import 'node-libs-react-native/globals';
-import Storybook from './storybook';
-export default Storybook;
+// import 'node-libs-react-native/globals';
+// import Storybook from './storybook';
+// export default Storybook;
+
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import FontLoader from './component/font-loader';
+import Welcome from '../src/view/welcome';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() =>
+            this.props.navigation.navigate('Details', {
+              store: {
+                itemId: 85,
+                otherParam: 'anything you want here',
+              },
+            })
+          }
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    /* 2. Get the param, provide a fallback value if not available */
+    const { navigation } = this.props;
+    const store = navigation.getParam('store', {});
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Text>itemId: {store.itemId}</Text>
+        <Text>otherParam: {store.otherParam}</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => navigation.push('MyModal')}
+        />
+      </View>
+    );
+  }
+}
+
+class ModalScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={{ fontSize: 30 }}>This is a modal!</Text>
+        <Button
+          onPress={() => this.props.navigation.goBack()}
+          title="Dismiss"
+        />
+      </View>
+    );
+  }
+}
+
+const MainStack = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+  },
+  {
+    // headerMode: 'none',
+  }
+);
+
+const RootStack = createStackNavigator(
+  {
+    Main: {
+      screen: MainStack,
+    },
+    MyModal: {
+      screen: ModalScreen,
+    },
+  },
+  {
+    mode: 'modal',
+    headerMode: 'none',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <FontLoader>
+        <RootStack />
+      </FontLoader>
+    );
+  }
+}
