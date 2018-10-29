55fe058d9bbbe9afb9fb0f0a8ea532064117f932
Tankred Hase <mail@tankredhase.de>
2018-10-29 15:41:17 +0100

    Use PasswordCard in RestorePasswordView

--- src/view/restore-password.js
+++ src/view/restore-password.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
-import Background from '../component/background';
+import { SplitBackground } from '../component/background';
 import MainContent from '../component/main-content';
 import { H1Text } from '../component/text';
 import { Header } from '../component/header';
 import { Button, BackButton, GlasButton } from '../component/button';
-import { InputField } from '../component/field';
-import Card from '../component/card';
-import { FormSubText, FormStretcher } from '../component/form';
+import { PasswordCard } from '../component/password-entry';
+import { color } from '../component/style';
 
 //
 // Restore Wallet Password View
@@ -23,42 +22,26 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: 20,
   },
-  card: {
-    maxHeight: 350,
-    maxWidth: 680,
-    paddingLeft: 45,
-    paddingRight: 45,
-    paddingBottom: 50,
-  },
 });
 
 const RestorePasswordView = ({ store, wallet, nav }) => (
-  <Background image="purple-gradient-bg">
+  <SplitBackground image="purple-gradient-bg" bottom={color.blackDark}>
     <Header>
       <BackButton onPress={() => nav.goSelectSeed()} />
       <Button disabled onPress={() => {}} />
     </Header>
     <MainContent style={styles.content}>
-      <View>
-        <H1Text style={styles.title}>Restore wallet</H1Text>
-      </View>
-      <Card style={styles.card}>
-        <FormSubText>Please enter your password.</FormSubText>
-        <FormStretcher>
-          <InputField
-            style={styles.input}
-            placeholder="Password"
-            secureTextEntry={true}
-            autoFocus={true}
-            value={store.wallet.password}
-            onChangeText={password => wallet.setPassword({ password })}
-            onSubmitEditing={() => wallet.restoreWallet()}
-          />
-        </FormStretcher>
-      </Card>
+      <H1Text style={styles.title}>Restore wallet</H1Text>
+      <PasswordCard
+        copy="Please enter your password."
+        placeholder="Password"
+        password={store.wallet.password}
+        onChangeText={password => wallet.setPassword({ password })}
+        onSubmitEditing={() => wallet.restoreWallet()}
+      />
       <GlasButton onPress={() => wallet.restoreWallet()}>Restore</GlasButton>
     </MainContent>
-  </Background>
+  </SplitBackground>
 );
 
 RestorePasswordView.propTypes = {
