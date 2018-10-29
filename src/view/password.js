52d648f4e4d653af2c6a0440aba52c9fbce5a495
Tankred Hase <mail@tankredhase.de>
2018-10-29 15:39:12 +0100

    Use PasswordCard design in PasswordView

--- src/view/password.js
+++ src/view/password.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
-import Background from '../component/background';
+import { SplitBackground } from '../component/background';
 import MainContent from '../component/main-content';
 import { H1Text } from '../component/text';
 import { GlasButton } from '../component/button';
-import { InputField } from '../component/field';
-import Card from '../component/card';
-import { FormSubText, FormStretcher } from '../component/form';
+import { PasswordCard } from '../component/password-entry';
+import { color } from '../component/style';
 
 //
 // Password View
@@ -22,38 +21,22 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: 20,
   },
-  card: {
-    maxHeight: 350,
-    maxWidth: 680,
-    paddingLeft: 45,
-    paddingRight: 45,
-    paddingBottom: 50,
-  },
 });
 
 const PasswordView = ({ store, wallet }) => (
-  <Background image="purple-gradient-bg">
+  <SplitBackground image="purple-gradient-bg" bottom={color.blackDark}>
     <MainContent style={styles.content}>
-      <View>
-        <H1Text style={styles.title}>Unlock wallet</H1Text>
-      </View>
-      <Card style={styles.card}>
-        <FormSubText>Please enter your password.</FormSubText>
-        <FormStretcher>
-          <InputField
-            style={styles.input}
-            placeholder="Password"
-            secureTextEntry={true}
-            autoFocus={true}
-            value={store.wallet.password}
-            onChangeText={password => wallet.setPassword({ password })}
-            onSubmitEditing={() => wallet.checkPassword()}
-          />
-        </FormStretcher>
-      </Card>
+      <H1Text style={styles.title}>Unlock wallet</H1Text>
+      <PasswordCard
+        copy="Please enter your password."
+        placeholder="Password"
+        password={store.wallet.password}
+        onChangeText={password => wallet.setPassword({ password })}
+        onSubmitEditing={() => wallet.checkPassword()}
+      />
       <GlasButton onPress={() => wallet.checkPassword()}>Unlock</GlasButton>
     </MainContent>
-  </Background>
+  </SplitBackground>
 );
 
 PasswordView.propTypes = {
