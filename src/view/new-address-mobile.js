49bd7a24880a8c1cafd12e03860703e03533edb2
Tankred Hase <mail@tankredhase.de>
2018-11-21 16:05:51 +0000

    Cleanup NewAddressMobile screen

--- src/view/new-address-mobile.js
+++ src/view/new-address-mobile.js
@@ -4,7 +4,7 @@ import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 import { SplitBackground } from '../component/background';
 import MainContent from '../component/main-content';
-import { CopyOnboardText, Text } from '../component/text';
+import { CopyOnboardText, CopyText } from '../component/text';
 import { CopyButton, GlasButton } from '../component/button';
 import { CopiedNotification } from '../component/notification';
 import CopyDarkIcon from '../../src/asset/icon/copy-dark';
@@ -12,16 +12,6 @@ import QRCode from '../component/qrcode';
 import { color } from '../component/style';
 
 const styles = StyleSheet.create({
-  qrWrapper: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    paddingTop: 90,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   content: {
     justifyContent: 'space-between',
   },
@@ -40,11 +30,9 @@ const styles = StyleSheet.create({
 
 const NewAddressView = ({ store, invoice, info }) => (
   <SplitBackground image="purple-gradient-bg" bottom={color.blackDark}>
-    <View style={styles.qrWrapper}>
-      <QRCode size={100}>{store.walletAddressUri}</QRCode>
-    </View>
     <MainContent style={styles.content}>
       <CopySection />
+      <QRCode size={130}>{store.walletAddressUri}</QRCode>
       <View style={styles.buttons}>
         <CopyButton
           onPress={() => invoice.toClipboard({ text: store.walletAddress })}
@@ -80,12 +68,12 @@ const copyStyles = StyleSheet.create({
   },
   title: {
     textAlign: 'center',
-    marginTop: 20,
+    marginTop: 60,
   },
   copyTxt: {
     textAlign: 'center',
     marginTop: 10,
-    maxWidth: 300,
+    maxWidth: 250,
   },
 });
 
@@ -94,10 +82,10 @@ const CopySection = () => (
     <CopyOnboardText style={copyStyles.title}>
       Your shiny new address
     </CopyOnboardText>
-    <Text style={copyStyles.copyTxt}>
+    <CopyText style={copyStyles.copyTxt}>
       Scan the QR code, or copy the address to send from another wallet or
-      exchange. Only Bitcoin works at the moment.
-    </Text>
+      exchange.
+    </CopyText>
   </View>
 );
 
