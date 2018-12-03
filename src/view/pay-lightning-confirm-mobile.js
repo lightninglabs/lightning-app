73af24695400b022b083321f40591661a45bac21
Tankred Hase <mail@tankredhase.de>
2018-12-03 16:59:45 +0000

    Implement PayLightningConfirmView for mobile

---
+++ src/view/pay-lightning-confirm-mobile.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { observer } from 'mobx-react';
+import PropTypes from 'prop-types';
+import Background from '../component/background';
+import MainContent from '../component/main-content';
+import { NamedField } from '../component/field';
+import { Header, Title } from '../component/header';
+import { CancelButton, BackButton, GlasButton } from '../component/button';
+import Card from '../component/card';
+import LightningBoltIcon from '../asset/icon/lightning-bolt';
+import { FormStretcher } from '../component/form';
+import {
+  BalanceLabel,
+  BalanceLabelNumeral,
+  BalanceLabelUnit,
+} from '../component/label';
+import { color } from '../component/style';
+
+const styles = StyleSheet.create({
+  description: {
+    paddingLeft: 50,
+    paddingRight: 50,
+  },
+  balance: {
+    marginBottom: 10,
+  },
+  numeral: {
+    color: color.blackText,
+  },
+  unit: {
+    color: color.blackText,
+  },
+  totalLbl: {
+    marginTop: 5,
+  },
+  note: {
+    marginTop: 5,
+    borderBottomWidth: 0,
+  },
+  confirmBtn: {
+    marginTop: 20,
+  },
+});
+
+const PayLightningConfirmView = ({ store, nav, payment }) => (
+  <Background color={color.purple}>
+    <Header color={color.purple}>
+      <BackButton onPress={() => nav.goPay()} />
+      <Title title="Lightning Confirmation">
+        <LightningBoltIcon height={12} width={6.1} />
+      </Title>
+      <CancelButton onPress={() => nav.goHome()} />
+    </Header>
+    <MainContent>
+      <Card>
+        <FormStretcher>
+          <BalanceLabel style={styles.balance}>
+            <BalanceLabelNumeral style={styles.numeral}>
+              {store.paymentAmountLabel}
+            </BalanceLabelNumeral>
+            <BalanceLabelUnit style={styles.unit}>
+              {store.unitLabel}
+            </BalanceLabelUnit>
+          </BalanceLabel>
+          <NamedField name="Fee">
+            {store.paymentFeeLabel} {store.unitLabel}
+          </NamedField>
+          <NamedField name="Total" style={styles.totalLbl}>
+            {store.paymentTotalLabel} {store.unitLabel}
+          </NamedField>
+          {store.payment.note ? (
+            <NamedField name="Note" style={styles.note}>
+              {store.payment.note}
+            </NamedField>
+          ) : null}
+        </FormStretcher>
+      </Card>
+    </MainContent>
+    <GlasButton onPress={() => payment.payLightning()}>Confirm</GlasButton>
+  </Background>
+);
+
+PayLightningConfirmView.propTypes = {
+  store: PropTypes.object.isRequired,
+  nav: PropTypes.object.isRequired,
+  payment: PropTypes.object.isRequired,
+};
+
+export default observer(PayLightningConfirmView);
