5c031d8719922aedd53e4dea74b041ac34b450fb
Tankred Hase <mail@tankredhase.de>
2018-12-03 18:42:24 +0000

    Implement white corners on QR code scanner

--- src/component/qrcode-scanner.js
+++ src/component/qrcode-scanner.js
@@ -5,6 +5,10 @@ import PropTypes from 'prop-types';
 import Text from './text';
 import { color } from './style';
 
+//
+// QR Code Scanner
+//
+
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
@@ -51,11 +55,14 @@ export default class QRCodeScanner extends React.Component {
       );
     } else {
       return (
-        <BarCodeScanner
-          onBarCodeScanned={this.props.onQRCodeScanned}
-          barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
-          style={styles.wrapper}
-        />
+        <View style={styles.wrapper}>
+          <BarCodeScanner
+            onBarCodeScanned={this.props.onQRCodeScanned}
+            barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+            style={styles.wrapper}
+          />
+          <Corners />
+        </View>
       );
     }
   }
@@ -64,3 +71,59 @@ export default class QRCodeScanner extends React.Component {
 QRCodeScanner.propTypes = {
   onQRCodeScanned: PropTypes.func.isRequired,
 };
+
+//
+// Corners
+//
+
+const cornerStyles = StyleSheet.create({
+  wrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  corners: {
+    height: 250,
+    width: 250,
+    justifyContent: 'space-between',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  corner: {
+    height: 30,
+    width: 30,
+    borderColor: color.white,
+  },
+  topLeft: {
+    borderTopWidth: 3,
+    borderLeftWidth: 3,
+  },
+  topRight: {
+    borderTopWidth: 3,
+    borderRightWidth: 3,
+  },
+  bottomLeft: {
+    borderBottomWidth: 3,
+    borderLeftWidth: 3,
+  },
+  bottomRight: {
+    borderBottomWidth: 3,
+    borderRightWidth: 3,
+  },
+});
+
+const Corners = () => (
+  <View style={[StyleSheet.absoluteFill, cornerStyles.wrapper]}>
+    <View style={cornerStyles.corners}>
+      <View style={cornerStyles.row}>
+        <View style={[cornerStyles.corner, cornerStyles.topLeft]} />
+        <View style={[cornerStyles.corner, cornerStyles.topRight]} />
+      </View>
+      <View style={cornerStyles.row}>
+        <View style={[cornerStyles.corner, cornerStyles.bottomLeft]} />
+        <View style={[cornerStyles.corner, cornerStyles.bottomRight]} />
+      </View>
+    </View>
+  </View>
+);
