2cd4186c51091c0b009c87dbccd784f8265f26d7
Tankred Hase <mail@tankredhase.de>
2018-12-18 16:04:07 +0000

    Implement channel detail mobile view

---
+++ src/view/channel-detail-mobile.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { observer } from 'mobx-react';
+import PropTypes from 'prop-types';
+import { SplitBackground } from '../component/background';
+import MainContent from '../component/main-content';
+import { Header, Title } from '../component/header';
+import { DetailField } from '../component/field';
+import { Button, BackButton, ButtonText } from '../component/button';
+import Card from '../component/card';
+import { color } from '../component/style';
+
+const styles = StyleSheet.create({
+  btnWrapper: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  deleteBtn: {
+    marginTop: 15,
+    minHeight: 50,
+  },
+  deleteBtnText: {
+    color: color.pinkSig,
+    fontSize: 12,
+  },
+});
+
+const ChannelDetailView = ({ store, nav }) => (
+  <SplitBackground color={color.blackDark} bottom={color.whiteBg}>
+    <Header>
+      <BackButton onPress={() => nav.goChannels()} />
+      <Title title="Channel Detail" />
+      <Button disabled onPress={() => {}} />
+    </Header>
+    <MainContent>
+      <Card>
+        <DetailField name="Funding Transaction ID">
+          {store.selectedChannel.fundingTxId}
+        </DetailField>
+        {store.selectedChannel.closingTxId ? (
+          <DetailField name="Closing Transaction ID">
+            {store.selectedChannel.closingTxId}
+          </DetailField>
+        ) : null}
+        <DetailField name="Remote Node Public Key">
+          {store.selectedChannel.remotePubkey}
+        </DetailField>
+        <DetailField name="Status">
+          {store.selectedChannel.statusLabel}
+        </DetailField>
+        <DetailField name="Capacity">
+          {store.selectedChannel.capacityLabel} {store.unitLabel}
+        </DetailField>
+        <DetailField name="Balance">
+          {store.selectedChannel.localBalanceLabel} {store.unitLabel}
+        </DetailField>
+        <View style={styles.btnWrapper}>
+          <Button
+            style={styles.deleteBtn}
+            onPress={() => nav.goChannelDelete()}
+          >
+            <ButtonText style={styles.deleteBtnText}>CLOSE CHANNEL</ButtonText>
+          </Button>
+        </View>
+      </Card>
+    </MainContent>
+  </SplitBackground>
+);
+
+ChannelDetailView.propTypes = {
+  store: PropTypes.object.isRequired,
+  nav: PropTypes.object.isRequired,
+};
+
+export default observer(ChannelDetailView);
