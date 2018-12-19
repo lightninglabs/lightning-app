c3291572c84409b09a138e8e940b4816dfad81f1
Tankred Hase <mail@tankredhase.de>
2018-12-19 15:16:02 +0000

    Cleanup payment failed mobile styles

--- src/view/payment-failed-mobile.js
+++ src/view/payment-failed-mobile.js
@@ -1,46 +1,58 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 import Background from '../component/background';
 import MainContent from '../component/main-content';
 import { CopyOnboardText, CopyText } from '../component/text';
-import { FormStretcher } from '../component/form';
-import { Button, ButtonText, GlasButton } from '../component/button';
+import { Button, ButtonText, SmallGlasButton } from '../component/button';
 import LightningErrorIcon from '../../src/asset/icon/lightning-error';
-import { color } from '../component/style';
+import { createStyles, maxWidth } from '../component/media-query';
+import { color, font, smallBreakWidth } from '../component/style';
 
-const styles = StyleSheet.create({
+const baseStyles = {
+  content: {
+    justifyContent: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
   headerTxt: {
     marginTop: 25,
     marginBottom: 10,
   },
+  copyTxt: {
+    textAlign: 'center',
+  },
   retryBtn: {
     marginTop: 5,
-    marginBottom: 20,
+    marginBottom: 10,
   },
-});
+};
+
+const styles = createStyles(
+  baseStyles,
+
+  maxWidth(smallBreakWidth, {
+    headerTxt: {
+      fontSize: font.sizeXXL - 5,
+    },
+  })
+);
 
 const PaymentFailedView = ({ channel, nav }) => (
   <Background color={color.blackDark}>
-    <MainContent>
-      <FormStretcher>
-        <LightningErrorIcon height={115 * 0.8} width={60 * 0.8} />
-        <CopyOnboardText style={styles.headerTxt}>
-          No route found
-        </CopyOnboardText>
-        <CopyText>{"You'll need to manually create a channel."}</CopyText>
-      </FormStretcher>
-      <GlasButton onPress={() => channel.initCreate()}>
-        Create channel
-      </GlasButton>
-      <Button
-        style={styles.retryBtn}
-        onPress={() => nav.goPayLightningConfirm()}
-      >
-        <ButtonText>Try again</ButtonText>
-      </Button>
+    <MainContent style={styles.content}>
+      <LightningErrorIcon height={115 * 0.8} width={60 * 0.8} />
+      <CopyOnboardText style={styles.headerTxt}>No route found</CopyOnboardText>
+      <CopyText style={styles.copyTxt}>
+        {"You'll need to manually create a channel."}
+      </CopyText>
     </MainContent>
+    <SmallGlasButton onPress={() => channel.initCreate()}>
+      Create channel
+    </SmallGlasButton>
+    <Button style={styles.retryBtn} onPress={() => nav.goPayLightningConfirm()}>
+      <ButtonText>Try again</ButtonText>
+    </Button>
   </Background>
 );
 
