a580e0f139303ff86ee29c091cd704a91f7fb3df
Tankred Hase <mail@tankredhase.de>
2019-01-16 16:26:40 +0700

    Implement notification wrapper component

---
+++ src/component/notification-wrapper.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { observer } from 'mobx-react';
+import PropTypes from 'prop-types';
+import { NotificationBar } from './notification';
+
+//
+// Notification Wrapper
+//
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+});
+
+const NotificationWrapper = ({ store, children }) => {
+  const { lastNotification, displayNotification } = store;
+  return (
+    <View style={styles.wrapper}>
+      <View style={StyleSheet.absoluteFill}>{children}</View>
+      <NotificationBar
+        notification={lastNotification}
+        display={displayNotification}
+      />
+    </View>
+  );
+};
+
+NotificationWrapper.propTypes = {
+  store: PropTypes.object.isRequired,
+  children: PropTypes.node,
+};
+
+export default observer(NotificationWrapper);
