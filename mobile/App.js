1e00bb97db758f15348530f1f82bdf70d9adb045
Tankred Hase <mail@tankredhase.de>
2019-02-12 17:28:23 +0700

    Load intl polypill on Android

--- mobile/App.js
+++ mobile/App.js
@@ -1,4 +1,9 @@
 import 'node-libs-react-native/globals';
+import { Platform } from 'react-native';
+if (Platform.OS === 'android') {
+  require('intl');
+  require('intl/locale-data/jsonp/en-US');
+}
 
 import App from '../src/view/main-mobile';
 
