8f15a44d1b8ce5d9c135be594194c2265fbe6641
Tankred Hase <mail@tankredhase.de>
2019-02-13 12:03:24 +0700

    Add polypill comment to App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,5 +1,8 @@
 import 'node-libs-react-native/globals';
 import { Platform } from 'react-native';
+
+// Polyfill ECMAScript Internationalization API on Android
+// See: https://github.com/facebook/react-native/issues/19410
 if (Platform.OS === 'android') {
   require('intl');
   require('intl/locale-data/jsonp/en-US');
