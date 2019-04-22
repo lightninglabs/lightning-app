3fbbaf23f8e5ddd854758d44bf0381e8d1659611
Tankred Hase <mail@tankredhase.de>
2019-04-22 14:59:12 +0800

    Integrate dropdown in notification-wrapper

--- src/component/notification-wrapper.js
+++ src/component/notification-wrapper.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
-import { NotificationBar } from './notification';
+import DropdownAlert from 'react-native-dropdownalert';
+import { Alert } from './notification';
+import { SmallSpinner } from './spinner';
+import { color, font } from './style';
+import { NOTIFICATION_DELAY } from '../config';
 
 //
 // Notification Wrapper
@@ -12,23 +16,50 @@ const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
   },
+  dropdown: {
+    backgroundColor: color.blackDark,
+  },
+  text: {
+    fontFamily: 'OpenSans Regular',
+    fontSize: font.sizeXS,
+    lineHeight: font.lineHeightXS,
+    color: color.white,
+  },
+  alert: {
+    alignSelf: 'center',
+    marginLeft: 15,
+    marginRight: 2,
+  },
+  spinner: {
+    alignSelf: 'flex-end',
+    marginRight: 15,
+    marginBottom: 6,
+  },
 });
 
-const NotificationWrapper = ({ store, children }) => {
-  const { lastNotification, displayNotification } = store;
-  return (
-    <View style={styles.wrapper}>
-      <NotificationBar
-        notification={lastNotification}
-        display={displayNotification}
-      />
-      {children}
-    </View>
-  );
-};
+const NotificationWrapper = ({ store, notify, children }) => (
+  <View style={styles.wrapper}>
+    {children}
+    <DropdownAlert
+      ref={ref => notify.setDropdown(ref)}
+      containerStyle={styles.dropdown}
+      messageStyle={styles.text}
+      renderImage={() => (
+        <Alert
+          type={(store.lastNotification || {}).type}
+          style={styles.alert}
+        />
+      )}
+      showCancel={(store.lastNotification || {}).waiting}
+      renderCancel={() => <SmallSpinner style={styles.spinner} />}
+      closeInterval={NOTIFICATION_DELAY}
+    />
+  </View>
+);
 
 NotificationWrapper.propTypes = {
   store: PropTypes.object.isRequired,
+  notify: PropTypes.object.isRequired,
   children: PropTypes.node,
 };
 
