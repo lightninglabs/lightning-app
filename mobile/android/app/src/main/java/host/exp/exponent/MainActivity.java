4ed297651a3c3151f5979c0c25449cd114e58064
Tankred Hase <mail@tankredhase.de>
2019-05-14 12:31:00 -0700

    Initial commit of android assets after ejecting to expokit

---
+++ mobile/android/app/src/main/java/host/exp/exponent/MainActivity.java
@@ -0,0 +1,45 @@
+package host.exp.exponent;
+
+import android.os.Bundle;
+
+import com.facebook.react.ReactPackage;
+
+import java.util.List;
+
+import expo.core.interfaces.Package;
+import host.exp.exponent.generated.DetachBuildConstants;
+import host.exp.exponent.experience.DetachActivity;
+
+public class MainActivity extends DetachActivity {
+
+  @Override
+  public String publishedUrl() {
+    return "exp://exp.host/@tanx/lightning-app";
+  }
+
+  @Override
+  public String developmentUrl() {
+    return DetachBuildConstants.DEVELOPMENT_URL;
+  }
+
+  @Override
+  public List<ReactPackage> reactPackages() {
+    return ((MainApplication) getApplication()).getPackages();
+  }
+
+  @Override
+  public List<Package> expoPackages() {
+    return ((MainApplication) getApplication()).getExpoPackages();
+  }
+
+  @Override
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
+
+  @Override
+  public Bundle initialProps(Bundle expBundle) {
+    // Add extra initialProps here
+    return expBundle;
+  }
+}
---
+++ mobile/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -0,0 +1,46 @@
+package host.exp.exponent.generated;
+
+import com.facebook.common.internal.DoNotStrip;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import host.exp.exponent.BuildConfig;
+import host.exp.exponent.Constants;
+
+@DoNotStrip
+public class AppConstants {
+
+  public static final String VERSION_NAME = "2.10.8";
+  public static String INITIAL_URL = "exp://exp.host/@tanx/lightning-app";
+  public static final String SHELL_APP_SCHEME = "lightning";
+  public static final String RELEASE_CHANNEL = "default";
+  public static boolean SHOW_LOADING_VIEW_IN_SHELL_APP = true;
+  public static boolean ARE_REMOTE_UPDATES_ENABLED = false;
+  public static final List<Constants.EmbeddedResponse> EMBEDDED_RESPONSES;
+  public static boolean FCM_ENABLED = false;
+
+  static {
+    List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
+
+    // ADD EMBEDDED RESPONSES HERE
+    // START EMBEDDED RESPONSES
+    // END EMBEDDED RESPONSES
+    EMBEDDED_RESPONSES = embeddedResponses;
+  }
+
+  // Called from expoview/Constants
+  public static Constants.ExpoViewAppConstants get() {
+    Constants.ExpoViewAppConstants constants = new Constants.ExpoViewAppConstants();
+    constants.VERSION_NAME = VERSION_NAME;
+    constants.INITIAL_URL = INITIAL_URL;
+    constants.SHELL_APP_SCHEME = SHELL_APP_SCHEME;
+    constants.RELEASE_CHANNEL = RELEASE_CHANNEL;
+    constants.SHOW_LOADING_VIEW_IN_SHELL_APP = SHOW_LOADING_VIEW_IN_SHELL_APP;
+    constants.ARE_REMOTE_UPDATES_ENABLED = ARE_REMOTE_UPDATES_ENABLED;
+    constants.EMBEDDED_RESPONSES = EMBEDDED_RESPONSES;
+    constants.ANDROID_VERSION_CODE = BuildConfig.VERSION_CODE;
+    constants.FCM_ENABLED = FCM_ENABLED;
+    return constants;
+  }
+}
