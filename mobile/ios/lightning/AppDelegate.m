8d7ced1e03338cad4a215d0ca86cf757c72cd0b4
Tankred Hase <mail@tankredhase.de>
2019-05-14 12:31:26 -0700

    Initial commit of ios assets after ejecting to expokit

---
+++ mobile/ios/lightning/AppDelegate.m
@@ -0,0 +1,15 @@
+// Copyright 2015-present 650 Industries. All rights reserved.
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+// Put your app delegate methods here. Remember to also call methods from EXStandaloneAppDelegate superclass
+// in order to keep Expo working. See example below.
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
