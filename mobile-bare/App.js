84f6e08b18ce448e67bfba5cdc2b485209383dc4
Tankred Hase <mail@tankredhase.de>
2019-06-17 14:17:12 -0500

    Configure expo-font to load .ttf files

--- mobile-bare/App.js
+++ mobile-bare/App.js
@@ -1,5 +1,10 @@
 import 'node-libs-react-native/globals';
-import { Platform } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
+import * as Font from 'expo-font';
+
+// Configure expo-font to load .ttf files
+// See: https://github.com/expo/expo/tree/master/packages/expo-font
+StyleSheet.setStyleAttributePreprocessor('fontFamily', Font.processFontFamily);
 
 // Polyfill ECMAScript Internationalization API on Android
 // See: https://github.com/facebook/react-native/issues/19410
