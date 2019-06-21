a881a5ff3d605fdfbb53f6354ba9ce05f0dd394f
Tankred Hase <mail@tankredhase.de>
2019-06-20 21:25:21 -0500

    Patch after react-native link react-native-svg

--- mobile-bare/android/app/src/main/java/com/lightningapp/MainApplication.java
+++ mobile-bare/android/app/src/main/java/com/lightningapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.lightningapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -38,6 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new SvgPackage(),
           new RNGestureHandlerPackage(),
           new ModuleRegistryAdapter(mModuleRegistryProvider)
       );
