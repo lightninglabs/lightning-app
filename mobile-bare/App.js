0899f4da9de59a8911cb053e1994f0d92472e6bd
Tankred Hase <mail@tankredhase.de>
2019-06-20 22:57:09 -0500

    Fix Intl not being found on Android

--- mobile-bare/App.js
+++ mobile-bare/App.js
@@ -1,17 +1,13 @@
 import 'node-libs-react-native/globals';
-import { Platform, StyleSheet } from 'react-native';
-import * as Font from 'expo-font';
-
-// Configure expo-font to load .ttf files
-// See: https://github.com/expo/expo/tree/master/packages/expo-font
-StyleSheet.setStyleAttributePreprocessor('fontFamily', Font.processFontFamily);
 
 // Polyfill ECMAScript Internationalization API on Android
 // See: https://github.com/facebook/react-native/issues/19410
-if (Platform.OS === 'android') {
-  require('intl');
-  require('intl/locale-data/jsonp/en-US');
-}
+import 'intl';
+import 'intl/locale-data/jsonp/en-US';
+
+import { StyleSheet } from 'react-native';
+import * as Font from 'expo-font';
+StyleSheet.setStyleAttributePreprocessor('fontFamily', Font.processFontFamily);
 
 import App from '../src/view/main-mobile';
 
