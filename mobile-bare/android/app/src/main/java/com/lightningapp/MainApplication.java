4b0349d86660726151b9bb9c5902e591c1c90018
Tankred Hase <mail@tankredhase.de>
2019-07-01 16:09:25 +0200

    Patch from react-native link react-native-camera

--- mobile-bare/android/app/src/main/java/com/lightningapp/MainApplication.java
+++ mobile-bare/android/app/src/main/java/com/lightningapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.lightningapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.oblador.keychain.KeychainPackage;
 import com.horcrux.svg.SvgPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -40,6 +41,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNCameraPackage(),
           new KeychainPackage(),
           new SvgPackage(),
           new RNGestureHandlerPackage(),
