3ec44628482755a8f4b3de105be2486aed9a27a7
Tankred Hase <mail@tankredhase.de>
2019-07-02 14:59:08 +0200

    Remove unused expo modules from BasePackageList.java

--- mobile-bare/android/app/src/main/java/com/lightningapp/generated/BasePackageList.java
+++ mobile-bare/android/app/src/main/java/com/lightningapp/generated/BasePackageList.java
@@ -7,15 +7,13 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
-        new expo.modules.barcodescanner.BarCodeScannerPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.localauthentication.LocalAuthenticationPackage(),
         new expo.modules.permissions.PermissionsPackage(),
-        new expo.modules.random.RandomPackage(),
-        new expo.modules.securestore.SecureStorePackage()
+        new expo.modules.random.RandomPackage()
     );
   }
 }
