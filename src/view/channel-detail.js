f514d6c53b2b23c64974881fa38b3954ecb0e26a
Tankred Hase <mail@tankredhase.de>
2019-08-16 13:18:02 +0200

    Closes #1228

--- src/view/channel-detail.js
+++ src/view/channel-detail.js
@@ -52,7 +52,7 @@ const ChannelDetailView = ({ store, nav }) => (
           {store.selectedChannel.statusLabel}
         </DetailField>
         {store.selectedChannel.timeTilAvailable ? (
-          <DetailField name="Time Til Available">
+          <DetailField name="Time Till Available">
             {store.selectedChannel.timeTilAvailable}
           </DetailField>
         ) : null}
