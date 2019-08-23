753354777991ffca4a02e3e66b29b47e6eaaf2f3
Tankred Hase <mail@tankredhase.de>
2019-08-23 19:53:33 +0200

    Add NamedFieldSelect story

--- stories/component/field-story.js
+++ stories/component/field-story.js
@@ -4,6 +4,7 @@ import { action } from '@storybook/addon-actions';
 import {
   InputField,
   NamedField,
+  NamedFieldSelect,
   DetailField,
   AmountInputField,
 } from '../../src/component/field';
@@ -13,6 +14,11 @@ storiesOf('Field', module)
     <InputField placeholder="Input Field" onChangeText={action('input')} />
   ))
   .add('Named Field', () => <NamedField name="Label">Named field</NamedField>)
+  .add('Named Field Select', () => (
+    <NamedFieldSelect name="Label" onPress={action('select')}>
+      Named field Select
+    </NamedFieldSelect>
+  ))
   .add('Detail Field', () => (
     <DetailField name="Label">Detail field</DetailField>
   ))
